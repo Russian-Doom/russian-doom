a29fca87d1bf9e439293d27f2de258e382721be0
JNechaevsky <julia.nechaevskaya@live.com>
2017-03-30 10:27:38 +0300

    TNTEM cheat code works again

--- src/doom/st_stuff.c
+++ src/doom/st_stuff.c
@@ -441,7 +441,7 @@ void ST_refreshBackground(void)
 }
 
 // [crispy] adapted from boom202s/M_CHEAT.C:467-498
-/*
+
 static int ST_cheat_massacre()
 {
     int killcount = 0;
@@ -476,7 +476,7 @@ static int ST_cheat_massacre()
 
     return killcount;
 } 
-*/
+
 
 // Respond to keyboard input events,
 //  intercept cheats.
@@ -580,6 +580,7 @@ ST_Responder (event_t* ev)
       // [JN] В несколько упрощенном варианте, без счетчика монстров
       else if (cht_CheckCheat(&cheat_massacre, ev->data2))
       {
+        int killcount = ST_cheat_massacre();
         plyr->message = DEH_String(STSTR_MASSACRE);
     
         // M_snprintf(msg, sizeof(msg), "%s%d %s%s%s %s",
