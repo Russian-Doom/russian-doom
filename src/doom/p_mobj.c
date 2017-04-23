0ee82d54e65614942e52d13f39cb3d174afdd604
JNechaevsky <julia.nechaevskaya@live.com>
2017-04-23 22:09:22 +0300

    [Doom] Fixed Mancubus fireball clipping
    https://doomwiki.org/wiki/Mancubus_fireball_clipping

--- src/doom/p_mobj.c
+++ src/doom/p_mobj.c
@@ -207,21 +207,48 @@ void P_XYMovement (mobj_t* mo)
     do
     {
     fixed_t ptryx, ptryy;
-    
-    if (xmove > MAXMOVE/2 || ymove > MAXMOVE/2)
-	{
-	    ptryx = mo->x + xmove/2;
-	    ptryy = mo->y + ymove/2;
-	    xmove >>= 1;
-	    ymove >>= 1;
-	}
-	else
-	{
-	    ptryx = mo->x + xmove;
-	    ptryy = mo->y + ymove;
-	    xmove = ymove = 0;
-	}
-		
+
+    // killough 8/9/98: fix bug in original Doom source:
+    // Large negative displacements were never considered.
+    // This explains the tendency for Mancubus fireballs
+    // to pass through walls.
+
+    // [JN] Исправление бага: https://doomwiki.org/wiki/Mancubus_fireball_clipping
+    // Исключительно для одиночной игры, т.к. вызывает рассинхронизацию демозаписией.
+
+    if (singleplayer)
+    {
+        if (xmove > MAXMOVE/2 || ymove > MAXMOVE/2 || xmove < -MAXMOVE/2 || ymove < -MAXMOVE/2)
+        {
+            ptryx = mo->x + xmove/2;
+            ptryy = mo->y + ymove/2;
+            xmove >>= 1;
+            ymove >>= 1;
+        }
+        else
+        {
+            ptryx = mo->x + xmove;
+            ptryy = mo->y + ymove;
+            xmove = ymove = 0;
+        }
+    }
+    else
+    {
+        if (xmove > MAXMOVE/2 || ymove > MAXMOVE/2)
+        {
+            ptryx = mo->x + xmove/2;
+            ptryy = mo->y + ymove/2;
+            xmove >>= 1;
+            ymove >>= 1;
+        }
+        else
+        {
+            ptryx = mo->x + xmove;
+            ptryy = mo->y + ymove;
+            xmove = ymove = 0;
+        }
+    }
+
 	if (!P_TryMove (mo, ptryx, ptryy))
 	{
 	    // blocked move
