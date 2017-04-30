9539e2fcb0395591e7d4448028a764c3adb0a011
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-04-30 14:07:28 +0300

    [DOS] Fixed crashes with IDMUS
    Thanks Fabian!

--- src_dos/st_stuff.c
+++ src_dos/st_stuff.c
@@ -613,8 +613,9 @@ ST_Responder (event_t* ev)
 	{
 	  musnum = mus_runnin + (buf[0]-'0')*10 + buf[1]-'0' - 1;
 	  
-	  if (((buf[0]-'0')*10 + buf[1]-'0') > 35)
-	    plyr->message = STSTR_NOMUS;
+	  // [crispy] prevent crash with IDMUS00
+	  if (((buf[0]-'0')*10 + buf[1]-'0') > 35 || musnum < mus_runnin)
+        plyr->message = STSTR_NOMUS;
 	  else
 	    S_ChangeMusic(musnum, 1);
 	}
@@ -622,7 +623,8 @@ ST_Responder (event_t* ev)
 	{
 	  musnum = mus_e1m1 + (buf[0]-'1')*9 + (buf[1]-'1');
 	  
-	  if (((buf[0]-'1')*9 + buf[1]-'1') > 31)
+	  // [crispy] prevent crash with IDMUS0x or IDMUSx0
+	  if (((buf[0]-'1')*9 + buf[1]-'1') > 31 || buf[0] < '1' || buf[1] < '1')
 	    plyr->message = STSTR_NOMUS;
 	  else
 	    S_ChangeMusic(musnum, 1);
