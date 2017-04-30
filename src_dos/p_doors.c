80998ba60747d107a3a3b257606cb2cfd0b5cf4a
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-04-30 13:03:44 +0300

    [DOS] Two fixes for fast doors
    1) Fast doors make two closing sounds
    2) Fast doors reopening with wrong sound
    
    Thanks Fabian!

--- src_dos/p_doors.c
+++ src_dos/p_doors.c
@@ -122,8 +122,8 @@ void T_VerticalDoor (vldoor_t* door)
 	      case blazeClose:
 		door->sector->specialdata = NULL;
 		P_RemoveThinker (&door->thinker);  // unlink and free
-		S_StartSound((mobj_t *)&door->sector->soundorg,
-			     sfx_bdcls);
+        // [crispy] fix "fast doors make two closing sounds"
+		// S_StartSound((mobj_t *)&door->sector->soundorg,sfx_bdcls);
 		break;
 		
 	      case normal:
@@ -148,6 +148,12 @@ void T_VerticalDoor (vldoor_t* door)
 	      case blazeClose:
 	      case close:		// DO NOT GO BACK UP!
 		break;
+
+        // [crispy] fix "fast doors reopening with wrong sound"
+        case blazeRaise:
+        door->direction = 1;
+        S_StartSound(&door->sector->soundorg, sfx_bdopn);
+        break;        
 		
 	      default:
 		door->direction = 1;
