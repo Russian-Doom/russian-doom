2539e82f869d94d275e200f84bf686dbea69f5fb
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-05-10 16:21:24 +0300

    [DOS ] Show E2M9 on the intermission screen
    if it has been completed.
    (https://github.com/fabiangreffrath/crispy-doom/commit/766de3901073cdb3244e973357a0a22c4845bf93)
    
    Thanks Fabian!

--- src_dos/wi_stuff.c
+++ src_dos/wi_stuff.c
@@ -596,6 +596,14 @@ void WI_drawAnimatedBack(void)
 	    V_DrawPatch(a->loc.x, a->loc.y, FB, a->p[a->ctr]);
     }
 
+    // [crispy] show Fortress of Mystery if it has been completed
+    if (wbs->epsd == 1 && wbs->didsecret)
+    {
+        a = &anims[1][7];
+ 
+        V_DrawPatch(a->loc.x, a->loc.y, 0, a->p[2]);
+    }    
+
 }
 
 //
