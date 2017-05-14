d3b59cfe0aa079d8c76e4225e7694879e9284c5f
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-05-14 14:21:20 +0300

    [DOS] Global source code conversation ANSI > OEM 866
    Good old times of keyrus.com TSR ...

--- src_dos/i_sound.h
+++ src_dos/i_sound.h
@@ -2,7 +2,8 @@
 // Copyright (C) 1993-1996 id Software, Inc.
 // Copyright (C) 1993-2008 Raven Software
 // Copyright (C) 2016-2017 Alexey Khokholov (Nuke.YKT)
-// Copyright (C) 2017 Alexandre-Xavier Labonté-Lamoureux
+// Copyright (C) 2017 Alexandre-Xavier Labonte-Lamoureux
+// Copyright (C) 2017 Julian Nechaevsky
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
