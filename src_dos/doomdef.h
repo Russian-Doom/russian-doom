35d1c9a59dea25006b452bea7ab433e2747185f3
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-05-21 16:38:33 +0300

    [DOS] Cosmetic code changes (doomdef.h)

--- src_dos/doomdef.h
+++ src_dos/doomdef.h
@@ -41,14 +41,14 @@ enum { VERSION =  109 };
 // For resize of screen, at start of game.
 // It will not work dynamically, see visplanes.
 //
-#define	BASE_WIDTH		320
+#define BASE_WIDTH      320
 
 // It is educational but futile to change this
 //  scaling e.g. to 2. Drawing of status bar,
 //  menues etc. is tied to the scale implied
 //  by the graphics.
-#define	SCREEN_MUL		1
-#define	INV_ASPECT_RATIO	0.625 // 0.75, ideally
+#define SCREEN_MUL          1
+#define INV_ASPECT_RATIO    0.625 // 0.75, ideally
 
 // Defines suck. C sucks.
 // C++ might sucks for OOP, but it sure is a better C.
@@ -58,16 +58,17 @@ enum { VERSION =  109 };
 #define SCREENHEIGHT 200
 //(int)(SCREEN_MUL*BASE_WIDTH*INV_ASPECT_RATIO) //200
 
-#define	FRACBITS		16
-#define	FRACUNIT		(1<<FRACBITS)
+#define FRACBITS    16
+#define FRACUNIT    (1<<FRACBITS)
+
 typedef int fixed_t;
 
 
 // The maximum number of players, multiplayer/networking.
-#define MAXPLAYERS		4
+#define MAXPLAYERS  4
 
 // State updates, number of tics / second.
-#define TICRATE		35
+#define TICRATE     35
 
 // The current state of the game: whether we are
 // playing, gazing at the intermission screen,
@@ -85,12 +86,12 @@ typedef enum
 //
 
 // Skill flags.
-#define	MTF_EASY		1
-#define	MTF_NORMAL		2
-#define	MTF_HARD		4
+#define MTF_EASY    1
+#define MTF_NORMAL  2
+#define MTF_HARD    4
 
 // Deaf monsters/do not react to sound.
-#define	MTF_AMBUSH		8
+#define MTF_AMBUSH  8
 
 typedef enum
 {
@@ -100,8 +101,6 @@ typedef enum
     sk_hard,
     sk_nightmare
 } skill_t;
-
-
 
 
 //
@@ -115,11 +114,8 @@ typedef enum
     it_blueskull,
     it_yellowskull,
     it_redskull,
-    
     NUMCARDS
-    
 } card_t;
-
 
 
 // The defined weapons,
@@ -136,25 +132,22 @@ typedef enum
     wp_bfg,
     wp_chainsaw,
     wp_supershotgun,
-
     NUMWEAPONS,
-    
+
     // No pending weapon change.
     wp_nochange
-
 } weapontype_t;
 
 
 // Ammunition types defined.
 typedef enum
 {
-    am_clip,	// Pistol / chaingun ammo.
-    am_shell,	// Shotgun / double barreled shotgun.
-    am_cell,	// Plasma rifle, BFG.
-    am_misl,	// Missile launcher.
+    am_clip,    // Pistol / chaingun ammo.
+    am_shell,   // Shotgun / double barreled shotgun.
+    am_cell,    // Plasma rifle, BFG.
+    am_misl,    // Missile launcher.
     NUMAMMO,
-    am_noammo	// Unlimited for chainsaw / fist.	
-
+    am_noammo   // Unlimited for chainsaw / fist.	
 } ammotype_t;
 
 
@@ -168,9 +161,7 @@ typedef enum
     pw_allmap,
     pw_infrared,
     NUMPOWERS
-    
 } powertype_t;
-
 
 
 //
@@ -180,14 +171,11 @@ typedef enum
 //
 typedef enum
 {
-    INVULNTICS	= (30*TICRATE),
-    INVISTICS	= (60*TICRATE),
-    INFRATICS	= (120*TICRATE),
-    IRONTICS	= (60*TICRATE)
-    
+    INVULNTICS  = (30*TICRATE),
+    INVISTICS   = (60*TICRATE),
+    INFRATICS   = (120*TICRATE),
+    IRONTICS    = (60*TICRATE)
 } powerduration_t;
-
-
 
 
 //
@@ -195,57 +183,57 @@ typedef enum
 // This is the stuff configured by Setup.Exe.
 // Most key data are simple ascii (uppercased).
 //
-#define KEY_RIGHTARROW	0xae
-#define KEY_LEFTARROW	0xac
-#define KEY_UPARROW	0xad
-#define KEY_DOWNARROW	0xaf
-#define KEY_ESCAPE	27
-#define KEY_ENTER	13
-#define KEY_TAB		9
-#define KEY_F1		(0x80+0x3b)
-#define KEY_F2		(0x80+0x3c)
-#define KEY_F3		(0x80+0x3d)
-#define KEY_F4		(0x80+0x3e)
-#define KEY_F5		(0x80+0x3f)
-#define KEY_F6		(0x80+0x40)
-#define KEY_F7		(0x80+0x41)
-#define KEY_F8		(0x80+0x42)
-#define KEY_F9		(0x80+0x43)
-#define KEY_F10		(0x80+0x44)
-#define KEY_F11		(0x80+0x57)
-#define KEY_F12		(0x80+0x58)
+#define KEY_RIGHTARROW  0xae
+#define KEY_LEFTARROW   0xac
+#define KEY_UPARROW     0xad
+#define KEY_DOWNARROW   0xaf
+#define KEY_ESCAPE      27
+#define KEY_ENTER       13
+#define KEY_TAB         9
+#define KEY_F1          (0x80+0x3b)
+#define KEY_F2          (0x80+0x3c)
+#define KEY_F3          (0x80+0x3d)
+#define KEY_F4          (0x80+0x3e)
+#define KEY_F5          (0x80+0x3f)
+#define KEY_F6          (0x80+0x40)
+#define KEY_F7          (0x80+0x41)
+#define KEY_F8          (0x80+0x42)
+#define KEY_F9          (0x80+0x43)
+#define KEY_F10         (0x80+0x44)
+#define KEY_F11         (0x80+0x57)
+#define KEY_F12         (0x80+0x58)
 
-#define KEY_BACKSPACE	127
-#define KEY_PAUSE	0xff
+#define KEY_BACKSPACE   127
+#define KEY_PAUSE       0xff
 
-#define KEY_EQUALS	0x3d
-#define KEY_MINUS	0x2d
+#define KEY_EQUALS      0x3d
+#define KEY_MINUS       0x2d
 
-#define KEY_RSHIFT	(0x80+0x36)
-#define KEY_RCTRL	(0x80+0x1d)
-#define KEY_RALT	(0x80+0x38)
+#define KEY_RSHIFT      (0x80+0x36)
+#define KEY_RCTRL       (0x80+0x1d)
+#define KEY_RALT        (0x80+0x38)
 
-#define KEY_LALT	KEY_RALT
+#define KEY_LALT        KEY_RALT
 
-fixed_t	FixedMul (fixed_t a, fixed_t b);
-fixed_t	FixedDiv (fixed_t a, fixed_t b);
-fixed_t	FixedDiv2 (fixed_t a, fixed_t b);
+fixed_t FixedMul  (fixed_t a, fixed_t b);
+fixed_t FixedDiv  (fixed_t a, fixed_t b);
+fixed_t FixedDiv2 (fixed_t a, fixed_t b);
 
 #pragma aux FixedMul =	\
-	"imul ebx",			\
-	"shrd eax,edx,16"	\
-	parm	[eax] [ebx] \
-	value	[eax]		\
-	modify exact [eax edx]
+    "imul ebx",			\
+    "shrd eax,edx,16"	\
+    parm	[eax] [ebx] \
+    value	[eax]		\
+    modify exact [eax edx]
 
 #pragma aux FixedDiv2 =	\
-	"cdq",				\
-	"shld edx,eax,16",	\
-	"sal eax,16",		\
-	"idiv ebx"			\
-	parm	[eax] [ebx] \
-	value	[eax]		\
-	modify exact [eax edx]
+    "cdq",				\
+    "shld edx,eax,16",	\
+    "sal eax,16",		\
+    "idiv ebx"			\
+    parm	[eax] [ebx] \
+    value	[eax]		\
+    modify exact [eax edx]
 
 #define SHORT(x)	(x)
 #define LONG(x)		(x)
@@ -290,13 +278,9 @@ fixed_t	FixedDiv2 (fixed_t a, fixed_t b);
 //#include "p_tick.h"
 
 
-
-
 // Header, generated by sound utility.
 // The utility was written by Dave Taylor.
 //#include "sounds.h"
 
-
-
-
 #endif          // __DOOMDEF__
+
