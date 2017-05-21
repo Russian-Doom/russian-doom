3438f3860333deabf3afaf7bd05355984b0294cb
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-05-21 18:15:30 +0300

    [DOS] Cosmetic code changes (f_wipe.*)

--- src_dos/f_wipe.h
+++ src_dos/f_wipe.h
@@ -23,43 +23,22 @@
 #define __F_WIPE_H__
 
 //
-//                       SCREEN WIPE PACKAGE
+// SCREEN WIPE PACKAGE
 //
 
 enum
 {
     // simple gradual pixel change for 8-bit only
     wipe_ColorXForm,
-    
     // weird screen melt
     wipe_Melt,	
 
     wipe_NUMWIPES
 };
 
-int
-wipe_StartScreen
-( int		x,
-  int		y,
-  int		width,
-  int		height );
-
-
-int
-wipe_EndScreen
-( int		x,
-  int		y,
-  int		width,
-  int		height );
-
-
-int
-wipe_ScreenWipe
-( int		wipeno,
-  int		x,
-  int		y,
-  int		width,
-  int		height,
-  int		ticks );
+int wipe_StartScreen (int x, int y, int width, int height);
+int wipe_EndScreen (int x, int y, int width, int height);
+int wipe_ScreenWipe (int wipeno, int x, int y, int width, int height, int ticks);
 
 #endif
+
