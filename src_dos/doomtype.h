48a464bd043fcdce20e56e924085f3a162d949db
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-05-21 16:45:45 +0300

    [DOS] Cosmetic code changes (doomtype.h)

--- src_dos/doomtype.h
+++ src_dos/doomtype.h
@@ -25,6 +25,7 @@
 
 #ifndef __BYTEBOOL__
 #define __BYTEBOOL__
+
 // Fixed to use builtin bool type with C++.
 #ifdef __cplusplus
 typedef bool boolean;
@@ -34,20 +35,18 @@ typedef enum {false, true} boolean;
 typedef unsigned char byte;
 #endif
 
-#define MAXCHAR		((char)0x7f)
-#define MAXSHORT	((short)0x7fff)
+#define MAXCHAR ((char)0x7f)
+#define MAXSHORT    ((short)0x7fff)
 
 // Max pos 32-bit int.
-#define MAXINT		((int)0x7fffffff)	
-#define MAXLONG		((long)0x7fffffff)
-#define MINCHAR		((char)0x80)
-#define MINSHORT	((short)0x8000)
+#define MAXINT      ((int)0x7fffffff)	
+#define MAXLONG     ((long)0x7fffffff)
+#define MINCHAR     ((char)0x80)
+#define MINSHORT    ((short)0x8000)
 
 // Max negative 32-bit integer.
-#define MININT		((int)0x80000000)	
-#define MINLONG		((long)0x80000000)
-
-
-
+#define MININT      ((int)0x80000000)	
+#define MINLONG     ((long)0x80000000)
 
 #endif
+
