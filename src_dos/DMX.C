f0471c774654de4c56f03eef8f3b1aa00921245e
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-05-29 14:38:12 +0300

    [DOS] Added support for PC Speaker
    Applied:
    https://github.com/nukeykt/PCDoom-v2/commit/26e82912f0a14436f3337ac61b2aef75c2ea893a
    
    Many thanks to @nukeykt and @AXDOOMER!

--- src_dos/DMX.C
+++ src_dos/DMX.C
@@ -1,5 +1,6 @@
 //
-// Copyright (C) 2015 Alexey Khokholov (Nuke.YKT)
+// Copyright (C) 2015-2017 Alexey Khokholov (Nuke.YKT)
+// Copyright (C) 2005-2014 Simon Howard
 // Copyright (C) 2017 Alexandre-Xavier Labonte-Lamoureux
 // Copyright (C) 2017 Julian Nechaevsky
 //
@@ -14,25 +15,60 @@
 // GNU General Public License for more details.
 //
 
+#include "dmx.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "dmx.h"
 #include "doomdef.h"
 #include "fx_man.h"
 #include "music.h"
 #include "task_man.h"
 #include "mus2mid.h"
+#include "pcfx.h"
 
-void TSM_Install(int rate) 
-{
+unsigned short divisors[] = {
+    0,
+    6818, 6628, 6449, 6279, 6087, 5906, 5736, 5575,
+    5423, 5279, 5120, 4971, 4830, 4697, 4554, 4435,
+    4307, 4186, 4058, 3950, 3836, 3728, 3615, 3519,
+    3418, 3323, 3224, 3131, 3043, 2960, 2875, 2794,
+    2711, 2633, 2560, 2485, 2415, 2348, 2281, 2213,
+    2153, 2089, 2032, 1975, 1918, 1864, 1810, 1757,
+    1709, 1659, 1612, 1565, 1521, 1478, 1435, 1395,
+    1355, 1316, 1280, 1242, 1207, 1173, 1140, 1107,
+    1075, 1045, 1015,  986,  959,  931,  905,  879,
+     854,  829,  806,  783,  760,  739,  718,  697,
+     677,  658,  640,  621,  604,  586,  570,  553,
+     538,  522,  507,  493,  479,  465,  452,  439,
+     427,  415,  403,  391,  380,  369,  359,  348,
+     339,  329,  319,  310,  302,  293,  285,  276,
+     269,  261,  253,  246,  239,  232,  226,  219,
+     213,  207,  201,  195,  190,  184,  179,
+};
+
+typedef struct {
+    unsigned int length;
+    unsigned short priority;
+    unsigned short data[0x10000];
+} pcspkmuse_t;
+
+typedef struct {
+    unsigned short id;
+    unsigned short length;
+    unsigned char data[];
+} dmxpcs_t;
+
+pcspkmuse_t pcspkmuse;
+int pcshandle = 0;
+
+void TSM_Install(int rate) {
+
 }
 
 fx_blaster_config dmx_blaster;
-int  (*tsm_func)(void);
+int(*tsm_func)(void);
 task *tsm_task = NULL;
 
-void tsm_funch() 
-{
+void tsm_funch() {
     tsm_func();
 }
 
@@ -43,54 +79,38 @@ int mus_loop = 0;
 int dmx_mus_port = 0;
 int dmx_sdev = NumSoundCards;
 
-int TSM_NewService(int(*function)(void), int rate, int unk1, int unk2) 
-{
+int TSM_NewService(int(*function)(void), int rate, int unk1, int unk2) {
     tsm_func = function;
     tsm_task = TS_ScheduleTask(tsm_funch, rate, 1, NULL);
     TS_Dispatch();
     return 0;
 }
-
-void TSM_DelService(int unk1) 
-{
-    if (tsm_task) 
-    {
+void TSM_DelService(int unk1) {
+    if (tsm_task) {
         TS_Terminate(tsm_task);
     }
     tsm_task = NULL;
 }
-
-void TSM_Remove(void) 
-{
+void TSM_Remove(void) {
     TS_Shutdown();
 }
-
-void MUS_PauseSong(int handle) 
-{
+void MUS_PauseSong(int handle) {
     MUSIC_Pause();
 }
-
-void MUS_ResumeSong(int handle) 
-{
+void MUS_ResumeSong(int handle) {
     MUSIC_Continue();
 }
-
-void MUS_SetMasterVolume(int volume) 
-{
+void MUS_SetMasterVolume(int volume) {
     MUSIC_SetVolume(volume * 2);
 }
-
-int MUS_RegisterSong(void *data) 
-{
-    FILE            *mus;
-    FILE            *mid;
-    unsigned int    midlen;
-    unsigned short  len;
-
+int MUS_RegisterSong(void *data) {
+    FILE *mus;
+    FILE *mid;
+    unsigned int midlen;
+    unsigned short len;
     mus_data = NULL;
-
-    len = ((unsigned short*)data)[2] + ((unsigned short*)data)[3];
-
+    len = ((unsigned short*)data)[2]
+        + ((unsigned short*)data)[3];
     if (mid_data)
     {
         free(mid_data);
@@ -98,23 +118,18 @@ int MUS_RegisterSong(void *data)
     if (memcmp(data, "MThd", 4))
     {
         mus = fopen("temp.mus", "wb");
-
         if (!mus)
         {
             return 0;
         }
-
         fwrite(data, 1, len, mus);
         fclose(mus);
         mus = fopen("temp.mus", "rb");
-
         if (!mus)
         {
             return 0;
         }
-
         mid = fopen("temp.mid", "wb");
-
         if (!mid)
         {
             fclose(mus);
@@ -126,27 +141,22 @@ int MUS_RegisterSong(void *data)
             fclose(mus);
             return 0;
         }
-
         fclose(mid);
         fclose(mus);
         mid = fopen("temp.mid", "rb");
-
         if (!mid)
         {
             return 0;
         }
-
         fseek(mid, 0, SEEK_END);
         midlen = ftell(mid);
         rewind(mid);
         mid_data = malloc(midlen);
-
         if (!mid_data)
         {
             fclose(mid);
             return 0;
         }
-
         fread(mid_data, 1, midlen, mid);
         fclose(mid);
         mus_data = mid_data;
@@ -154,146 +164,124 @@ int MUS_RegisterSong(void *data)
         remove("temp.mus");
         return 0;
     }
-
     mus_data = data;
     return 0;
 }
-
-int MUS_UnregisterSong(int handle) 
-{
+int MUS_UnregisterSong(int handle) {
     return 0;
 }
-
-int MUS_QrySongPlaying(int handle) 
-{
+int MUS_QrySongPlaying(int handle) {
     return 0;
 }
-
-int MUS_StopSong(int handle) 
-{
+int MUS_StopSong(int handle) {
     long status = MUSIC_StopSong();
     return (status != MUSIC_Ok);
 }
-
-int MUS_ChainSong(int handle, int next) 
-{
+int MUS_ChainSong(int handle, int next) {
     mus_loop = (next == handle);
     return 0;
 }
 
-int MUS_PlaySong(int handle, int volume) 
-{
+int MUS_PlaySong(int handle, int volume) {
     long status;
     if (mus_data == NULL)
     {
         return 1;
     }
-
     status = MUSIC_PlaySong((unsigned char*)mus_data, mus_loop);
     MUSIC_SetVolume(volume * 2);
-
-    // [JN] Fixed bug when music was playing/hearable when it's volume set to "0"
-    if (volume == 0)
-    MUS_PauseSong(handle);
-    else
-    MUS_ResumeSong(handle);
-
     return (status != MUSIC_Ok);
 }
 
-int SFX_PlayPatch(void *vdata, int pitch, int sep, int vol, int unk1, int unk2) 
-{
+int SFX_PlayPatch(void *vdata, int pitch, int sep, int vol, int unk1, int unk2) {
+    unsigned int rate;
+    unsigned long len;
     unsigned char *data = (unsigned char*)vdata;
-    unsigned int   type = (data[1] << 8) | data[0];
-    unsigned int   rate = (data[3] << 8) | data[2];
-    unsigned long   len = (data[7] << 24) | (data[6] << 16) | (data[5] << 8) | data[4];
-
-    if (type != 3) {
-        return -1;
+    unsigned int type = (data[1] << 8) | data[0];
+    dmxpcs_t *dmxpcs = (dmxpcs_t*)vdata;
+    unsigned short i;
+    if (type == 0)
+    {
+        pcspkmuse.length = dmxpcs->length * 2;
+        pcspkmuse.priority = 100;
+        for (i = 0; i < dmxpcs->length; i++)
+        {
+            pcspkmuse.data[i] = divisors[dmxpcs->data[i]];
+        }
+        pcshandle = PCFX_Play((PCSound *)&pcspkmuse, 100, 0);
+        return pcshandle | 0x8000;
     }
-    if (len <= 48) {
-        return -1;
+    else if (type == 3)
+    {
+        rate = (data[3] << 8) | data[2];
+        len = (data[7] << 24) | (data[6] << 16) | (data[5] << 8) | data[4];
+        if (len <= 48) {
+            return -1;
+        }
+        len -= 32;
+        return FX_PlayRaw(data + 24, len, rate, ((pitch - 128) * 2400) / 128, vol * 2, ((254 - sep) * vol) / 63, ((sep)* vol) / 63, 100, 0);
     }
-
-    len -= 32;
-    return FX_PlayRaw(data + 24, len, rate, ((pitch - 128) * 2400) / 128, vol * 2, ((254 - sep) * vol) / 63, ((sep)* vol) / 63, 100, 0);
 }
-
-void SFX_StopPatch(int handle) 
-{
+void SFX_StopPatch(int handle) {
+    if (handle & 0x8000)
+    {
+        PCFX_Stop(handle & 0x7fff);
+        return;
+    }
     FX_StopSound(handle);
 }
-
-int SFX_Playing(int handle) 
-{
+int SFX_Playing(int handle) {
+    if (handle & 0x8000)
+    {
+        return PCFX_SoundPlaying(handle & 0x7fff);
+    }
     return FX_SoundActive(handle);
 }
-
-void SFX_SetOrigin(int handle, int pitch, int sep, int vol) 
-{
+void SFX_SetOrigin(int handle, int  pitch, int sep, int vol) {
+    if (handle & 0x8000)
+    {
+        return;
+    }
     FX_SetPan(handle, vol * 2, ((254 - sep) * vol) / 63, ((sep)* vol) / 63);
     FX_SetPitch(handle, ((pitch - 128) * 2400) / 128);
 }
-
-int GF1_Detect(void) 
-{
+int GF1_Detect(void) {
     return 0; //FIXME
 }
-
-void GF1_SetMap(void *data, int len) 
-{
+void GF1_SetMap(void *data, int len) {
     FILE *ini = fopen("ULTRAMID.INI", "wb");
-
-    if (ini) 
-    {
+    if (ini) {
         fwrite(data, 1, len, ini);
         fclose(ini);
     }
 }
-
-int SB_Detect(int *port, int *irq, int *dma, int *unk) 
-{
-    if (FX_GetBlasterSettings(&dmx_blaster)) 
-    {
-        if (!port || !irq || !dma) 
-        {
+int SB_Detect(int *port, int *irq, int *dma, int *unk) {
+    if (FX_GetBlasterSettings(&dmx_blaster)) {
+        if (!port || !irq || !dma) {
             return -1;
         }
-
         dmx_blaster.Type = fx_SB;
         dmx_blaster.Address = *port;
         dmx_blaster.Interrupt = *irq;
         dmx_blaster.Dma8 = *dma;
     }
-
     return 0;
 }
-
-void SB_SetCard(int port, int irq, int dma)
-{ 
-    //FIXME
-}
-
-int AL_Detect(int *port, int *unk)
-{
+void SB_SetCard(int port, int irq, int dma) { } //FIXME
+int AL_Detect(int *port, int *unk) {
     return !AL_DetectFM();
 }
-
-void AL_SetCard(int port, void *data) 
-{
-    unsigned char   *cdata;
-    unsigned char   *tmb;
-    int             i;
-
+void AL_SetCard(int port, void *data) {
+    unsigned char *cdata;
+    unsigned char *tmb;
+    int i;
     cdata = (unsigned char *)data;
     tmb = malloc(13 * 256);
     memset(tmb, 0, 13 * 256);
-
     if (!tmb)
     {
         return;
     }
-
     for (i = 0; i < 128; i++)
     {
         tmb[i * 13 + 0] = cdata[8 + i * 36 + 4 + 0];
@@ -331,135 +319,101 @@ void AL_SetCard(int port, void *data)
                                 + cdata[8 + i * 36 + 4 + 14] + 12;
         tmb[(i + 35) * 13 + 12] = 0;
     }
-
     AL_RegisterTimbreBank(tmb);
     free(tmb);
 }
-
-int MPU_Detect(int *port, int *unk)
-{
-    if (port == NULL) 
-    {
+int MPU_Detect(int *port, int *unk) {
+    if (port == NULL) {
         return -1;
     }
-
     return MPU_Init(*port);
 }
-
-void MPU_SetCard(int port) 
-{
+void MPU_SetCard(int port) {
     dmx_mus_port = port;
 }
-
-int DMX_Init(int rate, int maxsng, int mdev, int sdev) 
-{
+int DMX_Init(int rate, int maxsng, int mdev, int sdev) {
     long status, device;
     dmx_sdev = sdev;
+    status = 0;
 
-    switch (mdev) 
-    {
-        case 0:
+    switch (mdev) {
+    case 0:
         device = NumSoundCards;
         break;
-
-        case AHW_ADLIB:
+    case AHW_ADLIB:
         device = Adlib;
         break;
-
-        case AHW_SOUND_BLASTER:
+    case AHW_SOUND_BLASTER:
         device = SoundBlaster;
         break;
-
-        case AHW_MPU_401:
+    case AHW_MPU_401:
         device = GenMidi;
         break;
-
-        case AHW_ULTRA_SOUND:
+    case AHW_ULTRA_SOUND:
         device = UltraSound;
         break;
-
-        default:
+    default:
         return -1;
         break;
     }
-
-    if (device == SoundBlaster) 
-    {
-        int MaxVoices;
-        int MaxBits;
-        int MaxChannels;
-
-        FX_SetupSoundBlaster(dmx_blaster, (int *)&MaxVoices, (int *)&MaxBits, (int *)&MaxChannels);
-    }
-
     status = MUSIC_Init(device, dmx_mus_port);
-
-    if (status != MUSIC_Ok)
-    {
-        return -1;
+    if (status == MUSIC_Ok) {
+        MUSIC_SetVolume(0);
     }
-    
-    MUSIC_SetVolume(0);
-    return 0;
+    if (sdev & AHW_PC_SPEAKER)
+    {
+        PCFX_Init();
+        PCFX_SetTotalVolume(255);
+        PCFX_UseLookup(0, 0);
+    }
+    return mdev | sdev;
 }
 
-void DMX_DeInit(void)
-{
+void DMX_DeInit(void) {
     MUSIC_Shutdown();
     FX_Shutdown();
+    PCFX_Shutdown();
     remove("ULTRAMID.INI");
-
     if (mid_data)
     {
         free(mid_data);
     }
 }
 
-void WAV_PlayMode(int channels, int samplerate) 
-{
+void WAV_PlayMode(int channels, int samplerate) {
     long device, status;
-
-    // char tmp[300];
-    switch (dmx_sdev) 
-    {
-        case 0:
+    char tmp[300];
+    switch (dmx_sdev) {
+    case 0:
         device = NumSoundCards;
         break;
-
-        case AHW_SOUND_BLASTER:
+    case AHW_SOUND_BLASTER:
         device = SoundBlaster;
         break;
-
-        case AHW_ULTRA_SOUND:
+    case AHW_ULTRA_SOUND:
         device = UltraSound;
         break;
-
-        default:
+    default:
         return;
         break;
     }
+    if (device == SoundBlaster) {
 
-    if (device == SoundBlaster) 
-    {
         int MaxVoices;
         int MaxBits;
         int MaxChannels;
 
         FX_SetupSoundBlaster(dmx_blaster, (int *)&MaxVoices, (int *)&MaxBits, (int *)&MaxChannels);
     }
-
     status = FX_Init(device, channels, 2, 16, samplerate);
     FX_SetVolume(255);
-    printf("Статус %i %i %i %i\n", device, device + 10, SoundBlaster, status);
 }
 
 int CODEC_Detect(int *a, int *b)
 {
     return 1;
 }
-
 int ENS_Detect(void)
 {
     return 1;
 }
-
