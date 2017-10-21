a9a31da7ad9282b90c3c7b2b0eaa9b90a1fc5b63
Julian Nechaevsky <julia.nechaevskaya@live.com>
2017-10-21 13:17:59 +0300

    Fixed FIRELAV* animation sequence
    Correct texture sequence is:
    - FIRELAV2
    - FIRELAV3
    - FIRELAVA
    
    This solves problem with missing animation sequence in case of using FIRELAV2 as a texture (very notable in the Ultimate Doom E4M5). But there are more to say, see below...

--- src/doom/p_spec.c
+++ src/doom/p_spec.c
@@ -121,7 +121,9 @@ animdef_t       animdefs[] =
     {true,  "BLODRIP4", "BLODRIP1", 8},
     {true,  "FIREWALL", "FIREWALA", 8},
     {true,  "GSTFONT3", "GSTFONT1", 8},
-    {true,  "FIRELAVA", "FIRELAV3", 8},
+    // [JN] Для восстановления последовательности 
+    // анимации, FIRELAV3 изменен на FIRELAV2.
+    {true,  "FIRELAVA", "FIRELAV2", 8}, 
     {true,  "FIREMAG3", "FIREMAG1", 8},
     {true,  "FIREBLU2", "FIREBLU1", 8},
     {true,  "ROCKRED3", "ROCKRED1", 8},
