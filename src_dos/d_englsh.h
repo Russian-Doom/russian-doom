dfb09c482d34208406ba3f8214bca2a42e9a4abf
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2018-02-11 15:32:25 +0300

    DOS: release version 1.3

--- src_dos/d_englsh.h
+++ src_dos/d_englsh.h
@@ -1141,7 +1141,7 @@
 
 
 // Port-specific strings
-#define STSTR_VERSION   "dthcbz 1>3 (d hfhpf,jnrt)"                 // Версия 1.3 (в разработке)
+#define STSTR_VERSION   "dthcbz 1>3 (11>02>2018)"                   // Версия 1.3 (11.02.2018)
 
 #define STSTR_KAADDED   "rk.xb gjkextys"                            // Ключи получены
 
