d9956e562916a4887d2a95c0715e20ca796350b0
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2018-03-09 11:05:57 +0300

    Doom: move brightmap initialization to separate files
    Applying mechanism is still a bit messy, but it's working fine. At least, this move is allowing to get rid of bunch of additional int-s.
    
    I'll probably will do it also for DOS version, but not very interested in doing this and Heretic and Hexen.

--- src/doom/r_segs.c
+++ src/doom/r_segs.c
@@ -31,33 +31,12 @@
 #include "r_sky.h"
 #include "g_game.h"
 #include "r_segs.h"
+#include "r_bmaps.h"
 
 // MACROS ------------------------------------------------------------------
 
 #define HEIGHTBITS  12
 #define HEIGHTUNIT  (1<<HEIGHTBITS)
-
-// PUBLIC DATA DEFINITIONS -------------------------------------------------
-
-// [JN] Wall brightmaps initialization
-extern int bmaptexture01, bmaptexture02, bmaptexture03, bmaptexture04, 
-bmaptexture05, bmaptexture06, bmaptexture07, bmaptexture08, bmaptexture09, 
-bmaptexture10, bmaptexture11, bmaptexture12, bmaptexture13, bmaptexture14, 
-bmaptexture15, bmaptexture16, bmaptexture17, bmaptexture18, bmaptexture19,
-bmaptexture20, bmaptexture21, bmaptexture22, bmaptexture23, bmaptexture24, 
-bmaptexture25, bmaptexture26, bmaptexture27, bmaptexture28, bmaptexture29, 
-bmaptexture30, bmaptexture31, bmaptexture32, bmaptexture33, bmaptexture34, 
-bmaptexture35, bmaptexture36, bmaptexture37, bmaptexture38, bmaptexture39, 
-bmaptexture40, bmaptexture41, bmaptexture42, bmaptexture43, bmaptexture44, 
-bmaptexture45, bmaptexture46, bmaptexture47, bmaptexture48, bmaptexture49, 
-bmaptexture50, bmaptexture51, bmaptexture52, bmaptexture53, bmaptexture54,
-bmaptexture55, bmaptexture56, bmaptexture57, bmaptexture58, bmaptexture59, 
-bmaptexture60, bmaptexture61, bmaptexture62, bmaptexture63, bmaptexture64, 
-bmaptexture65, bmaptexture66, bmaptexture67, bmaptexture68, bmaptexture69, 
-bmaptexture70, bmaptexture71, bmaptexture72, bmaptexture73, bmaptexture74,
-bmaptexture75, bmaptexture76, bmaptexture77, bmaptexture78, bmaptexture79, 
-bmaptexture80, bmaptexture81, bmaptexture82, bmaptexture83, bmaptexture84,
-bmap_terminator;
 
 // PUBLIC DATA DEFINITIONS -------------------------------------------------
 
