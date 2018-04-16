402f9acf8d7abb76ec89d4f25c1f7b0fc3210f44
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2018-04-16 22:54:07 +0300

    Remove the unreliable Software GL check
    Applied: https://github.com/chocolate-doom/chocolate-doom/pull/1024

--- src/i_video.c
+++ src/i_video.c
@@ -1247,30 +1247,6 @@ static void SetVideoMode(void)
     CreateUpscaledTexture(true);
 }
 
-
-static const char *hw_emu_warning = 
-"==============================================================================\n"
-"ВНИМАНИЕ: В вашей системе используется программная эмуляция аппратного\n"
-"ускорения, это может отрицательно сказаться на графической производительности.\n" 
-"==============================================================================\n";
-
-static void CheckGLVersion(void)
-{
-    const char * version;
-    typedef const GLubyte* (APIENTRY * glStringFn_t)(GLenum);
-    glStringFn_t glfp = (glStringFn_t)SDL_GL_GetProcAddress("glGetString");
-
-    if (glfp)
-    {
-        version = (const char *)glfp(GL_VERSION);
-
-        if (version && strstr(version, "Mesa"))
-        {
-            printf("%s", hw_emu_warning);
-        }
-    }
-}
-
 void I_InitGraphics(void)
 {
     SDL_Event dummy;
@@ -1321,10 +1297,6 @@ void I_InitGraphics(void)
     // on configuration.
     AdjustWindowSize();
     SetVideoMode();
-
-    // We might have poor performance if we are using an emulated
-    // HW accelerator. Check for Mesa and warn if we're using it.
-    CheckGLVersion();
 
     // Start with a clear black screen
     // (screen will be flipped after we set the palette)
