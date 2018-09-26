210daaaaa6a25d99603206aac18079ed05a14c89
JNechaevsky <julia.nechaevskaya@live.com>
2018-09-26 20:21:16 +0300

    Atari Jaguar: apply brightmap to SW2GARG

--- src/doom/r_bmaps.c
+++ src/doom/r_bmaps.c
@@ -77,6 +77,7 @@ void R_InitBrightmaps(void)
         bmaptexture23 = R_TextureNumForName("WOODGARG");
         bmaptexture39 = R_TextureNumForName("EXITSIGN");
         bmaptexture75 = R_TextureNumForName("CBLUE01");
+        bmaptexture88 = R_TextureNumForName("SW2GARG");
 
         // Apply terminator
         bmap_terminator = R_TextureNumForName("BIGDOOR2");
