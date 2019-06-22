481542cd3d7f87e69e70783238e546b0b7045dab
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2019-06-22 15:53:26 +0300

    Doom: fix status bar background in automap if screenblocks >=11 in wide mode
    Goddamit.

--- src/doom/st_stuff.c
+++ src/doom/st_stuff.c
@@ -485,8 +485,13 @@ void ST_Stop(void);
 
 void ST_refreshBackground(void)
 {
-    if (screenblocks >= 11 && !automapactive)
+#ifdef WIDESCREEN
+    if (!automapactive)
     return;
+#else
+    if (screenblocks >= 11 && !automapactive)
+    return;    
+#endif
 
     if (st_statusbaron)
     {
@@ -502,17 +507,11 @@ void ST_refreshBackground(void)
 
         V_RestoreBuffer();
 
-        // [JN] Do not call V_CopyRect in wide screen mode, because of
-        // different status bar drawing routines and effective screen size.
-        // Following condition preventing from black bars appering in some 
-        // cases like holding 'ENTER' on aspect ratio changing.
-#ifndef WIDESCREEN
         // [JN] Jaguar: use different height of status bar
         if (gamemission == jaguar)
         V_CopyRect(ST_X, 0, st_backing_screen, ST_WIDTH, ST_HEIGHT_JAG, ST_X, ST_Y_JAG);
         else
         V_CopyRect(ST_X, 0, st_backing_screen, ST_WIDTH, ST_HEIGHT, ST_X, ST_Y);
-#endif
     }
 }
 
