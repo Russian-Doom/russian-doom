71a5b4be315514001418380b6c3e94e8878ed520
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2019-07-04 23:34:18 +0300

    Classic: repair `d_iwad.h`

--- src_classic/src/d_iwad.h
+++ src_classic/src/d_iwad.h
@@ -24,7 +24,7 @@
 #define IWAD_MASK_DOOM    ((1 << doom)           \
                          | (1 << doom2)          \
                          | (1 << pack_tnt)       \
-                         | (1 << pack_plut)
+                         | (1 << pack_plut))
 #define IWAD_MASK_HERETIC (1 << heretic)
 #define IWAD_MASK_HEXEN   (1 << hexen)
 #define IWAD_MASK_STRIFE  (1 << strife)
