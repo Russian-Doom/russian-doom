c8f0db22ec94423ca2a61742c77205d0458a782e
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2019-07-07 10:04:25 +0300

    Classic: update automap while playing

--- src_classic/src/doom/d_main.c
+++ src_classic/src/doom/d_main.c
@@ -216,7 +216,11 @@ void D_Display (void)
 	if (!gametic)
 	    break;
 	if (automapactive)
+    {
+	    // [crispy] update automap while playing
+	    R_RenderPlayerView (&players[displayplayer]);
 	    AM_Drawer ();
+    }
 	if (wipe || (viewheight != SCREENHEIGHT && fullscreen))
 	    redrawsbar = true;
 	if (inhelpscreensstate && !inhelpscreens)
