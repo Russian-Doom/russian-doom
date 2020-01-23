8acec468887679c7b4b88d9bcd287c675e7af164
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2020-01-23 22:13:17 +0300

    Proper toggling by both 'enter' keys
    Thanks @bradharding, thanks @fabiangreffrath !

--- src/i_video.c
+++ src/i_video.c
@@ -396,17 +396,8 @@ static boolean ToggleFullScreenKeyShortcut(SDL_Keysym *sym)
 #if defined(__MACOSX__)
     flags |= (KMOD_LGUI | KMOD_RGUI);
 #endif
-    return sym->scancode == SDL_SCANCODE_RETURN && (sym->mod & flags) != 0;
-}
-
-// [JN] Alt + Keypad Enter (the Enter key on numeric keypad)
-static boolean ToggleFullScreenKeyShortcutNum(SDL_Keysym *sym)
-{
-    Uint16 flags = (KMOD_LALT | KMOD_RALT);
-#if defined(__MACOSX__)
-    flags |= (KMOD_LGUI | KMOD_RGUI);
-#endif
-    return sym->scancode == SDL_SCANCODE_KP_ENTER && (sym->mod & flags) != 0;
+    return (sym->scancode == SDL_SCANCODE_RETURN ||
+            sym->scancode == SDL_SCANCODE_KP_ENTER) && (sym->mod & flags) != 0;
 }
 
 static void I_ToggleFullScreen(void)
@@ -450,8 +441,7 @@ void I_GetEvent(void)
         switch (sdlevent.type)
         {
             case SDL_KEYDOWN:
-                if (ToggleFullScreenKeyShortcut(&sdlevent.key.keysym)
-                || (ToggleFullScreenKeyShortcutNum(&sdlevent.key.keysym)))
+                if (ToggleFullScreenKeyShortcut(&sdlevent.key.keysym))
                 {
                     I_ToggleFullScreen();
                     break;
