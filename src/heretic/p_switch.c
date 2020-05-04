8ec5549d095dfb3146439ba0ac726bc543322c19
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2020-05-04 12:25:44 +0300

    Heretic: p_switch.c - cleanup

--- src/heretic/p_switch.c
+++ src/heretic/p_switch.c
@@ -30,63 +30,10 @@
 //      CHANGE THE TEXTURE OF A WALL SWITCH TO ITS OPPOSITE
 //
 //==================================================================
-switchlist_t alphSwitchList[] = {
+switchlist_t alphSwitchList[] =
+{
     {"SW1OFF", "SW1ON", 1},
     {"SW2OFF", "SW2ON", 1},
-
-/*
-	{"SW1CTY",		"SW2CTY",	1},
-	{"SW1ORGRY",	"SW2ORGRY",	1},
-	{"SW1GRSTN",	"SW2GRSTN",	1},
-	{"SW1SNDP",		"SW2SNDP",	1},
-	{"SW1SPINE",	"SW2SPINE",	1},
-	{"SW1SQPEB",	"SW2SQPEB",	1},
-	{"SW1TRST1",	"SW2TRST1",	1},
-	{"SW1CSTL",		"SW2CSTL",	1},
-	{"SW1MOSS",		"SW2MOSS",	1},
-	{"SW1SNDSQ",	"SW2SNDSQ",	1},
-	{"SW1RED",		"SW2RED",	1},
-	{"SW1WOOD",		"SW2WOOD",	1},
-	{"SW1BROWN",	"SW2BROWN",	1},
-
-	{"SW1TRST2",	"SW2TRST2",	2},
-	{"SW1MSC",		"SW2MSC",	2},
-	{"SW1MSC2",		"SW2MSC2",	2},
-	{"SW1GRDMD",	"SW2GRDMD",	2},
-*/
-
-#if 0
-    {"SW1BRCOM", "SW2BRCOM", 1},
-    {"SW1BRN1", "SW2BRN1", 1},
-    {"SW1BRN2", "SW2BRN2", 1},
-    {"SW1BRNGN", "SW2BRNGN", 1},
-    {"SW1BROWN", "SW2BROWN", 1},
-    {"SW1COMM", "SW2COMM", 1},
-    {"SW1COMP", "SW2COMP", 1},
-    {"SW1DIRT", "SW2DIRT", 1},
-    {"SW1EXIT", "SW2EXIT", 1},
-    {"SW1GRAY", "SW2GRAY", 1},
-    {"SW1GRAY1", "SW2GRAY1", 1},
-    {"SW1METAL", "SW2METAL", 1},
-    {"SW1PIPE", "SW2PIPE", 1},
-    {"SW1SLAD", "SW2SLAD", 1},
-    {"SW1STARG", "SW2STARG", 1},
-    {"SW1STON1", "SW2STON1", 1},
-    {"SW1STON2", "SW2STON2", 1},
-    {"SW1STONE", "SW2STONE", 1},
-    {"SW1STRTN", "SW2STRTN", 1},
-
-    {"SW1BLUE", "SW2BLUE", 2},
-    {"SW1CMT", "SW2CMT", 2},
-    {"SW1GARG", "SW2GARG", 2},
-    {"SW1GSTON", "SW2GSTON", 2},
-    {"SW1HOT", "SW2HOT", 2},
-    {"SW1LION", "SW2LION", 2},
-    {"SW1SATYR", "SW2SATYR", 2},
-    {"SW1SKIN", "SW2SKIN", 2},
-    {"SW1VINE", "SW2VINE", 2},
-    {"SW1WOOD", "SW2WOOD", 2},
-#endif
     {"\0", "\0", 0}
 };
 
@@ -180,8 +127,6 @@ void P_ChangeSwitchTexture(line_t * line, int useAgain)
     texBot = sides[line->sidenum[0]].bottomtexture;
 
     sound = sfx_switch;
-    //if (line->special == 11) // EXIT SWITCH?
-    //      sound = sfx_swtchx;
 
     for (i = 0; i < numswitches * 2; i++)
         if (switchlist[i] == texTop)
