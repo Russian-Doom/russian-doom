52630edb9c3bb36c19bddc39a32453bace3e20d4
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2020-09-12 12:58:31 +0300

    Bump Russian Heretic version to 4.2

--- src/heretic/rd_lang.h
+++ src/heretic/rd_lang.h
@@ -732,7 +732,7 @@ extern char* txt_mlook_off;
 //
 
 // English
-#define TXT_VERSION "VERSION 4.1"
+#define TXT_VERSION "VERSION 4.2"
 #if defined (_WIN64)
 #define TXT_ARCH      " - (X64)"
 #elif defined (_WIN32)
@@ -740,10 +740,10 @@ extern char* txt_mlook_off;
 #else
 #define TXT_ARCH      ""
 #endif
-#define TXT_DATE      "2020-05-17"          // YYYY-MM-DD
+#define TXT_DATE      "2020-09-16"          // YYYY-MM-DD
 
 // Русский
-#define TXT_VERSION_RUS "DTHCBZ 4>1"        // ВЕРСИЯ 4.1
+#define TXT_VERSION_RUS "DTHCBZ 4>2"        // ВЕРСИЯ 4.2
 #if defined (_WIN64)
 #define TXT_ARCH_RUS      " - ([64)"        // x64
 #elif defined (_WIN32)
