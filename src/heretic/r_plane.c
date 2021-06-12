b5c4ae10795576bac000eb88dd648919077de732
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2021-06-12 09:23:22 +0300

    Heretic: even better invul sky handling

--- src/heretic/r_plane.c
+++ src/heretic/r_plane.c
@@ -455,7 +455,8 @@ void R_DrawPlanes (void)
 
                     if (invul_sky && !vanillaparm)
                     {
-                        if (players[consoleplayer].powers[pw_invulnerability])  
+                        if (players[consoleplayer].powers[pw_invulnerability] > BLINKTHRESHOLD
+                        || (players[consoleplayer].powers[pw_invulnerability] & 8))
                         {
                             // [JN] Invulnerability effect will colorize sky texture,
                             // with out any changes in sky texture light level.
