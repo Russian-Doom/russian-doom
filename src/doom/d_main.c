Russian-Doom/russian-doom

commit a54db5c54dd167df6ae529605e4eac736af05d77

Moved versions to build systems

src/doom/d_main.c
@@ -2459,7 +2459,7 @@ void D_DoomMain (void)
                            | FOREGROUND_INTENSITY);
 
     for (p = 0 ; p < 32 ; p++) DEH_printf(" ");
-    DEH_printf("Russian Doom " PACKAGE_VERSION);
+    DEH_printf("Russian Doom " BUILD_DOOM_VERSION);
     for (p = 0 ; p < 31 ; p++) DEH_printf(" ");
     DEH_printf("\n");
 
@@ -2469,7 +2469,7 @@ void D_DoomMain (void)
 #else
     // [JN] Just print an uncolored banner
     for (p = 0 ; p < 32 ; p++) DEH_printf(" ");
-    DEH_printf("Russian Doom " PACKAGE_VERSION);
+    DEH_printf("Russian Doom " PACKAGE_DOOM_VERSION);
     for (p = 0 ; p < 31 ; p++) DEH_printf(" ");
     DEH_printf("\n");
 #endif

src/heretic/d_main.c
@@ -1266,7 +1266,7 @@ void D_DoomMain(void)
                                                            | FOREGROUND_BLUE
                                                            | FOREGROUND_INTENSITY);
     for (p = 0 ; p < 32 ; p++) DEH_printf(" ");
-    DEH_printf("Russian Heretic " PACKAGE_VERSION);
+    DEH_printf("Russian Heretic " BUILD_HERETIC_VERSION);
     for (p = 0 ; p < 31 ; p++) DEH_printf(" ");
     DEH_printf("\n");
 
@@ -1277,7 +1277,7 @@ void D_DoomMain(void)
 #else
     // [JN] Just print an uncolored banner
     for (p = 0 ; p < 32 ; p++) DEH_printf(" ");
-    DEH_printf("Russian Heretic " PACKAGE_VERSION);
+    DEH_printf("Russian Heretic " PACKAGE_HERETIC_VERSION);
     for (p = 0 ; p < 31 ; p++) DEH_printf(" ");
     DEH_printf("\n");
 #endif 

src/hexen/h2_main.c
@@ -483,7 +483,7 @@ void D_DoomMain(void)
                                                            | FOREGROUND_BLUE
                                                            | FOREGROUND_INTENSITY);
     for (p = 0 ; p < 32 ; p++) ST_Message(" ");
-    ST_Message("Russian Hexen " PACKAGE_VERSION);
+    ST_Message("Russian Hexen " BUILD_HEXEN_VERSION);
     for (p = 0 ; p < 31 ; p++) ST_Message(" ");
     ST_Message("\n");
 
@@ -494,7 +494,7 @@ void D_DoomMain(void)
 #else
     // [JN] Just print an uncolored banner
     for (p = 0 ; p < 32 ; p++) ST_Message(" ");
-    ST_Message("Russian Hexen " PACKAGE_VERSION);
+    ST_Message("Russian Hexen " PACKAGE_HEXEN_VERSION);
     for (p = 0 ; p < 31 ; p++) ST_Message(" ");
     ST_Message("\n");
 #endif

CMakeLists.txt
@@ -3,6 +3,13 @@ cmake_minimum_required(VERSION 3.15)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake)
 
 project("Russian Doom" VERSION 5.0 LANGUAGES C NONE)
+set(BUILD_HERETIC_VERSION "4.3")
+set(BUILD_HEXEN_VERSION "4.1")
+set(BUILD_STRIFE_VERSION "0.0")
+set(BUILD_DOOM_DATE "2021-05-04") # YYYY-MM-DD
+set(BUILD_HERETIC_DATE "2021-01-03") # YYYY-MM-DD
+set(BUILD_HEXEN_DATE "2021-01-03") # YYYY-MM-DD
+set(BUILD_STRIFE_DATE "2021-00-00") # YYYY-MM-DD
 
 # Autotools variables
 set(top_srcdir ${CMAKE_CURRENT_SOURCE_DIR})
@@ -11,10 +18,15 @@ set(top_builddir ${CMAKE_CURRENT_BINARY_DIR})
 # AC_INIT variables
 set(PACKAGE_NAME "${PROJECT_NAME}")
 set(PACKAGE_TARNAME "russian-doom")
-set(PACKAGE_VERSION "${PROJECT_VERSION}")
 set(PACKAGE_STRING "${PROJECT_NAME} ${PROJECT_VERSION}")
 set(PACKAGE_COPYRIGHT "Copyright (C) 2016-2021")
 set(PACKAGE_LICENSE "GNU General Public License, version 2")
+
+set(BUILD_DOOM_VERSION "${PROJECT_VERSION}")
+string(REPLACE "." ">" BUILD_DOOM_VERSION_RUS ${BUILD_DOOM_VERSION})
+string(REPLACE "." ">" BUILD_HERETIC_VERSION_RUS ${BUILD_HERETIC_VERSION})
+string(REPLACE "." ">" BUILD_HEXEN_VERSION_RUS ${BUILD_HEXEN_VERSION})
+string(REPLACE "." ">" BUILD_STRIFE_VERSION_RUS ${BUILD_STRIFE_VERSION})
 
 # Check architecture
 if(WIN32)

cmake/config.h.cin
@@ -1,8 +1,22 @@
 #cmakedefine PACKAGE_NAME "@PACKAGE_NAME@"
 #cmakedefine PACKAGE_TARNAME "@PACKAGE_TARNAME@"
-#cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
 #cmakedefine PACKAGE_STRING "@PACKAGE_STRING@"
 #cmakedefine PROGRAM_PREFIX "@PROGRAM_PREFIX@"
+
+#cmakedefine BUILD_DOOM_VERSION "@BUILD_DOOM_VERSION@"
+#cmakedefine BUILD_HERETIC_VERSION "@BUILD_HERETIC_VERSION@"
+#cmakedefine BUILD_HEXEN_VERSION "@BUILD_HEXEN_VERSION@"
+#cmakedefine BUILD_STRIFE_VERSION "@BUILD_STRIFE_VERSION@"
+
+#cmakedefine BUILD_DOOM_VERSION_RUS "@BUILD_DOOM_VERSION_RUS@"
+#cmakedefine BUILD_HERETIC_VERSION_RUS "@BUILD_HERETIC_VERSION_RUS@"
+#cmakedefine BUILD_HEXEN_VERSION_RUS "@BUILD_HEXEN_VERSION_RUS@"
+#cmakedefine BUILD_STRIFE_VERSION_RUS "@BUILD_STRIFE_VERSION_RUS@"
+
+#cmakedefine BUILD_DOOM_DATE "@BUILD_DOOM_DATE@"
+#cmakedefine BUILD_HERETIC_DATE "@BUILD_HERETIC_DATE@"
+#cmakedefine BUILD_HEXEN_DATE "@BUILD_HEXEN_DATE@"
+#cmakedefine BUILD_STRIFE_DATE "@BUILD_STRIFE_DATE@"
 
 #cmakedefine HAVE_LIBSAMPLERATE
 #cmakedefine HAVE_LIBPNG

configure.ac
@@ -1,13 +1,41 @@
 AC_INIT(Russian Doom, 5.0, julian.nechaevsky@outlook.com,
         russian-doom)
 
+BUILD_HERETIC_VERSION="4.3"
+BUILD_HEXEN_VERSION="4.1"
+BUILD_STRIFE_VERSION="0.0"
+
+BUILD_DOOM_DATE="2021-05-04" # YYYY-MM-DD
+BUILD_HERETIC_DATE="2021-01-03" # YYYY-MM-DD
+BUILD_HEXEN_DATE="2021-01-03" # YYYY-MM-DD
+BUILD_STRIFE_DATE="2021-00-00" # YYYY-MM-DD
+
+BUILD_DOOM_VERSION=${PACKAGE_VERSION}
+BUILD_DOOM_VERSION_RUS=`echo $BUILD_DOOM_VERSION | sed 's/\./>/g'`
+BUILD_HERETIC_VERSION_RUS=`echo $BUILD_HERETIC_VERSION | sed 's/\./>/g'`
+BUILD_HEXEN_VERSION_RUS=`echo $BUILD_HEXEN_VERSION_VERSION | sed 's/\./>/g'`
+BUILD_STRIFE_VERSION_RUS=`echo $BUILD_STRIFE_VERSION_VERSION | sed 's/\./>/g'`
 PACKAGE_SHORTNAME=${PACKAGE_NAME% Doom}
 PACKAGE_SHORTDESC="Russian Doom, Freedoom, Heretic and Hexen"
 PACKAGE_COPYRIGHT="Copyright (C) 2016-2021"
 PACKAGE_LICENSE="GNU General Public License, version 2"
 PACKAGE_MAINTAINER="Julian Nechaevsky"
 PACKAGE_URL="https://jnechaevsky.github.io/rusdoom"
-# PACKAGE_ISSUES="https://github.com/chocolate-doom/chocolate-doom/issues"
+
+AC_DEFINE_UNQUOTED(BUILD_DOOM_VERSION, "$BUILD_DOOM_VERSION", "Doom build version")
+AC_DEFINE_UNQUOTED(BUILD_HERETIC_VERSION, "$BUILD_HERETIC_VERSION", "Heretic build version")
+AC_DEFINE_UNQUOTED(BUILD_HEXEN_VERSION, "$BUILD_HEXEN_VERSION", "Hexen build version")
+AC_DEFINE_UNQUOTED(BUILD_STRIFE_VERSION, "$BUILD_STRIFE_VERSION", "Strife build version")
+
+AC_DEFINE_UNQUOTED(BUILD_DOOM_VERSION_RUS, "$BUILD_DOOM_VERSION_RUS", "Doom build russian version")
+AC_DEFINE_UNQUOTED(BUILD_HERETIC_VERSION_RUS, "$BUILD_HERETIC_VERSION_RUS", "Heretic build russian version")
+AC_DEFINE_UNQUOTED(BUILD_HEXEN_VERSION_RUS, "$BUILD_HEXEN_VERSION_RUS", "Hexen build russian version")
+AC_DEFINE_UNQUOTED(BUILD_STRIFE_DATE, "$BUILD_STRIFE_DATE", "Strife build russian version")
+
+AC_DEFINE_UNQUOTED(BUILD_DOOM_DATE, "$BUILD_DOOM_DATE", "Doom build date")
+AC_DEFINE_UNQUOTED(BUILD_HERETIC_DATE, "$BUILD_HERETIC_DATE", "Heretic build date")
+AC_DEFINE_UNQUOTED(BUILD_HEXEN_DATE, "$BUILD_HEXEN_DATE", "Hexen build date")
+AC_DEFINE_UNQUOTED(BUILD_HERETIC_VERSION, "$BUILD_HERETIC_VERSION", "Strife build date")
 
 AC_CONFIG_AUX_DIR(autotools)
 AC_CANONICAL_HOST
@@ -133,7 +161,6 @@ AC_SUBST(PACKAGE_COPYRIGHT)
 AC_SUBST(PACKAGE_LICENSE)
 AC_SUBST(PACKAGE_MAINTAINER)
 AC_SUBST(PACKAGE_URL)
-AC_SUBST(PACKAGE_ISSUES)
 
 dnl Shut up the datarootdir warnings.
 AC_DEFUN([AC_DATAROOTDIR_CHECKED])

src/doom/rd_lang.h
@@ -2259,13 +2259,7 @@ extern char* ststr_massacre;
 #define CC_CYBER_RUS    "rb,thltvjy"            // Кибердемон
 #define CC_HERO_RUS     "yfi uthjq"             // Наш Герой
 
-
-//
-// Отображение версии проекта
-//
-
 // English
-#define STSTR_VERSION   "Version 5.0"
 #if defined (_WIN64)
 #define STSRT_ARCH      " - (x64)"
 #elif defined (_WIN32)
@@ -2273,10 +2267,8 @@ extern char* ststr_massacre;
 #else
 #define STSRT_ARCH      ""
 #endif
-#define STSRT_DATE      "2021-00-00"        // YYYY-MM-DD
 
 // Русский
-#define STSTR_VERSION_RUS   "dthcbz 5>0"    // Версия 5.0
 #if defined (_WIN64)
 #define STSRT_ARCH_RUS      " - ([64)"      // x64
 #elif defined (_WIN32)
@@ -2284,6 +2276,5 @@ extern char* ststr_massacre;
 #else
 #define STSRT_ARCH_RUS      ""              // ?  
 #endif
-
 
 #endif

src/doom/st_stuff.c
@@ -711,9 +711,10 @@ boolean ST_Responder (event_t* ev)
             // [JN] Отображение версии проекта
             else if (cht_CheckCheat(&cheat_version, ev->data2))
             {
-                M_snprintf(msg, sizeof(msg), "%s%s - %s",
-                english_language ? STSTR_VERSION : STSTR_VERSION_RUS,
-                english_language ? STSRT_ARCH : STSRT_ARCH_RUS, STSRT_DATE);
+                M_snprintf(msg, sizeof(msg), "%s %s%s - %s",
+                english_language ? "Version" : "dthcbz",
+                english_language ? BUILD_DOOM_VERSION : BUILD_DOOM_VERSION_RUS,
+                english_language ? STSRT_ARCH : STSRT_ARCH_RUS, BUILD_DOOM_DATE);
 
                 plyr->message_system = msg;
             }

commit d42f8dc538fe966353362796b25d5984200dc57b

Fixed build fail on Unix introduced in a54db5c54dd167df6ae529605e4eac736af05d77

src/doom/d_main.c
@@ -2469,7 +2469,7 @@ void D_DoomMain (void)
 #else
     // [JN] Just print an uncolored banner
     for (p = 0 ; p < 32 ; p++) DEH_printf(" ");
-    DEH_printf("Russian Doom " PACKAGE_DOOM_VERSION);
+    DEH_printf("Russian Doom " BUILD_DOOM_VERSION);
     for (p = 0 ; p < 31 ; p++) DEH_printf(" ");
     DEH_printf("\n");
 #endif

src/heretic/d_main.c
@@ -1277,7 +1277,7 @@ void D_DoomMain(void)
 #else
     // [JN] Just print an uncolored banner
     for (p = 0 ; p < 32 ; p++) DEH_printf(" ");
-    DEH_printf("Russian Heretic " PACKAGE_HERETIC_VERSION);
+    DEH_printf("Russian Heretic " BUILD_HERETIC_VERSION);
     for (p = 0 ; p < 31 ; p++) DEH_printf(" ");
     DEH_printf("\n");
 #endif 

src/hexen/h2_main.c
@@ -494,7 +494,7 @@ void D_DoomMain(void)
 #else
     // [JN] Just print an uncolored banner
     for (p = 0 ; p < 32 ; p++) ST_Message(" ");
-    ST_Message("Russian Hexen " PACKAGE_HEXEN_VERSION);
+    ST_Message("Russian Hexen " BUILD_HEXEN_VERSION);
     for (p = 0 ; p < 31 ; p++) ST_Message(" ");
     ST_Message("\n");
 #endif