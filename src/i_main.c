Russian-Doom/russian-doom

commit db0dabf36e4457253321f87802d894d73e133cc7

Use WinAPI to get exeDir instead of argv[0]

src/i_main.c
@@ -35,6 +35,7 @@
 #include "doomtype.h"
 #include "i_system.h"
 #include "m_argv.h"
+#include "m_misc.h"
 
 // [JN] Vanilla game mode available for all three games in RD
 boolean vanillaparm;
@@ -74,6 +75,24 @@ void RD_CreateWindowsConsole (void)
     wcscpy(cfi.FaceName, L"Consolas");
     SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
 }
+
+void M_SetExeDir(void)
+{
+    TCHAR dirname[MAX_PATH + 1];
+    DWORD dirname_len;
+    TCHAR *fp = NULL;
+
+    memset(dirname, 0, sizeof(dirname));
+    dirname_len = GetModuleFileName(NULL, dirname, MAX_PATH);
+    fp = &dirname[dirname_len];
+    while (dirname <= fp && *fp != DIR_SEPARATOR)
+    {
+        fp--;
+    }
+    *(fp + 1) = '\0';
+
+    exedir = M_StringDuplicate(dirname);
+}
 #endif
 
 
@@ -93,13 +112,14 @@ int main(int argc, char **argv)
     myargc = argc;
     myargv = argv;
 
-    M_SetExeDir();
     M_FindResponseFile();
 
     // Check for -devparm being activated
     devparm = M_CheckParm ("-devparm");
 
 #ifdef _WIN32
+    M_SetExeDir();
+
     // [JN] Create a console output on Windows for devparm mode.
     if (devparm)
     {

src/m_argv.c
@@ -271,12 +271,3 @@ char *M_GetExecutableName(void)
 }
 
 char *exedir = NULL;
-
-void M_SetExeDir(void)
-{
-    char *dirname;
-
-    dirname = M_DirName(myargv[0]);
-    exedir = M_StringJoin(dirname, DIR_SEPARATOR_S, NULL);
-    free(dirname);
-}

src/m_argv.h
@@ -32,7 +32,6 @@ extern  char**	myargv;
 
 // [Dasperal] exedir from chocolate
 extern char *exedir;
-void M_SetExeDir(void);
 
 // Returns the position of the given parameter
 // in the arg list (0 if not found).

commit 5b063a7f3566c6059728213620668af6a50d5c16

Unix: Fixed crash on startup introduced in db0dabf36e4457253321f87802d894d73e133cc7

src/i_main.c
@@ -63,9 +63,11 @@ void RD_CreateWindowsConsole (void)
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 }
+#endif
 
 void M_SetExeDir(void)
 {
+#ifdef _WIN32
     TCHAR dirname[MAX_PATH + 1];
     DWORD dirname_len;
     TCHAR *fp = NULL;
@@ -80,9 +82,14 @@ void M_SetExeDir(void)
     *(fp + 1) = '\0';
 
     exedir = M_StringDuplicate(dirname);
-}
-#endif
+#else
+    char *dirname;
 
+    dirname = M_DirName(myargv[0]);
+    exedir = M_StringJoin(dirname, DIR_SEPARATOR_S, NULL);
+    free(dirname);
+#endif
+}
 
 //
 // D_DoomMain()
@@ -100,14 +107,14 @@ int main(int argc, char **argv)
     myargc = argc;
     myargv = argv;
 
+    M_SetExeDir();
+
     M_FindResponseFile();
 
     // Check for -devparm being activated
     devparm = M_CheckParm ("-devparm");
 
 #ifdef _WIN32
-    M_SetExeDir();
-
     // [JN] Create a console output on Windows for devparm mode.
     if (devparm)
     {