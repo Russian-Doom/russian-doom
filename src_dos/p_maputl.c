53a2bdac1a9a5f8d00fa4d7a9a0cdd3a59e9ee42
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2021-07-24 18:48:53 +0300

    DOS: upgrade `P_InterceptVector`

--- src_dos/p_maputl.c
+++ src_dos/p_maputl.c
@@ -124,23 +124,39 @@ void P_MakeDivline (line_t *li, divline_t *dl)
     dl->dy = li->dy;
 }
 
-
-//
+// -----------------------------------------------------------------------------
 // P_InterceptVector
-// Returns the fractional intercept point
-// along the first divline.
-// This is only called by the addthings
-// and addlines traversers.
+// Returns the fractional intercept point along the first divline.
+// This is only called by the addthings and addlines traversers.
 //
-// killough 5/3/98: reformatted, cleaned up
-//
+// [JN] killough 5/3/98: reformatted, cleaned up
+// -----------------------------------------------------------------------------
+
 fixed_t P_InterceptVector (divline_t *v2, divline_t *v1)
 {
-  fixed_t den = FixedMul(v1->dy>>8, v2->dx) - FixedMul(v1->dx>>8, v2->dy);
-  return den ? FixedDiv((FixedMul((v1->x-v2->x)>>8, v1->dy) +
-                         FixedMul((v2->y-v1->y)>>8, v1->dx)), den) : 0;
-}
+    if (singleplayer)
+    {
+        // [JN] cph - no precision/overflow problems
+        int64_t den = (int64_t)v1->dy * v2->dx - (int64_t)v1->dx * v2->dy;
+        den >>= 16;
+        
+        if (!den)
+        {
+            return 0;
+        }
 
+        return (fixed_t)(((int64_t)(v1->x - v2->x) * v1->dy
+                        - (int64_t)(v1->y - v2->y) * v1->dx) / den);
+    }
+    else
+    {
+        // [JN] Original demo-safe code.
+        fixed_t den = FixedMul(v1->dy >> 8, v2->dx) - FixedMul(v1->dx >> 8, v2->dy);
+
+        return den ? FixedDiv((FixedMul((v1->x-v2->x) >> 8, v1->dy)
+                   + FixedMul((v2->y-v1->y) >> 8, v1->dx)), den) : 0;
+    }
+}
 
 //
 // P_LineOpening
