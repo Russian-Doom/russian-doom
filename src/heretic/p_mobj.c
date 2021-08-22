d2bfc8f28db7b263eb230a79069793b3441b0c28
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2021-08-22 13:01:22 +0300

    Heretic: fix wallrunning bug

--- src/heretic/p_mobj.c
+++ src/heretic/p_mobj.c
@@ -373,8 +373,18 @@ void P_XYMovement(mobj_t * mo)
         // [JN] Fix projectiles may sometimes pass though walls, altrough
         // it is rarely happening in Heretic. This fix is written by Lee Killough,
         // and it's same to: https://doomwiki.org/wiki/Mancubus_fireball_clipping
-        if ((xmove > MAXMOVE/2 || ymove > MAXMOVE/2) 
-        || (singleplayer && !vanillaparm && (xmove < -MAXMOVE/2 || ymove < -MAXMOVE/2)))
+        //
+        // Thanks to Jeff Doggett for simplifying!
+        //
+        // Additionally, wallrunning bug is fixed (mo->player condition):
+        // https://doomwiki.org/wiki/Wallrunning
+        //
+        // Thanks AXDOOMER and Brad Harding!
+
+        if (singleplayer && !vanillaparm ? 
+            mo->player ? ((xmove > MAXMOVE/2 || ymove > MAXMOVE/2) && (xmove < -MAXMOVE/2 || ymove < -MAXMOVE/2)) 
+                       : ((xmove > MAXMOVE/2 || ymove > MAXMOVE/2) || (xmove < -MAXMOVE/2 || ymove < -MAXMOVE/2))
+                       :  (xmove > MAXMOVE/2 || ymove > MAXMOVE/2))
         {
             ptryx = mo->x + xmove / 2;
             ptryy = mo->y + ymove / 2;
