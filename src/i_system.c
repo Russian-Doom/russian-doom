312817462310d030a40255c0167bf26747f4935e
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2021-10-20 22:24:28 +0300

    Windows 11: fix potentional black screen after exit

--- src/i_system.c
+++ src/i_system.c
@@ -201,7 +201,12 @@ void I_Quit (void)
         entry = entry->next;
     }
 
+    // [JN] Windows only: do not call clean up of all initialized 
+    // subsystems, as they should be freed earlier. Fixes potentional
+    // black screen after exit on Windows 11.
+#ifndef _WIN32
     SDL_Quit();
+#endif
 
     exit(0);
 }
