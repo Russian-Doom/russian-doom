404c875109c15e9f3d6e0a93cd6c228a71b49f29
Dasperal <dasperal1@gmail.com>
2022-01-09 22:35:56 +0300

    Update copyright

--- src/rd_keybinds.h
+++ src/rd_keybinds.h
@@ -1,5 +1,5 @@
 //
-// Copyright(C) 2020-2021 Dasperal
+// Copyright(C) 2020-2022 Dasperal
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
