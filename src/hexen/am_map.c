7cbc03ff04fefa5b488648ebe39fc9caf785d3ea
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2022-01-12 20:21:18 +0300

    Hexen: fix incorrect, limited zoom

--- src/hexen/am_map.c
+++ src/hexen/am_map.c
@@ -191,8 +191,8 @@ static void AM_findMinMaxBoundaries(void)
 
     a = FixedDiv(f_w << FRACBITS, max_w);
     b = FixedDiv(f_h << FRACBITS, max_h);
-    min_scale_mtof = a < b ? a : b;
 
+    min_scale_mtof = a < b ? a/2 : b/2;
     max_scale_mtof = FixedDiv(f_h << FRACBITS, 2 * PLAYERRADIUS);
 
 }
