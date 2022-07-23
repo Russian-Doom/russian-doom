f02ba7564b4b45c3e60e6df366ec3ad2fe76f967
Dasperal <dasperal1@gmail.com>
2022-07-23 23:16:00 +0300

    Cmake: Move some parts of script to subdirectories
    Specify sources of `gus-patches`, `win-libs` and `*-wads` targets explicitly

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,30 +1,41 @@
-function(add_copy_target)
-    set(ARGS NAME DESTINATION)
-    set(LIST_ARGS SOURCE)
-    cmake_parse_arguments(PARSE_ARGV 0 "ARG"
-        ""
-        "${ARGS}"
-        "${LIST_ARGS}"
+# add_copy_target(<target_name> SOURCE <file1> <...> [DESTINATION <dir>])
+function(add_copy_target target_name)
+    cmake_parse_arguments(PARSE_ARGV 1 "ARG"
+        ""            # List of Options
+        "DESTINATION" # List of Values
+        "SOURCE"      # List of Lists
     )
 
-    foreach(SOURCE_FILE_PATH ${ARG_SOURCE})
-        get_filename_component(DEST_FILE_NAME "${SOURCE_FILE_PATH}" NAME)
-        add_custom_command(OUTPUT "${ARG_DESTINATION}/${DEST_FILE_NAME}"
-            MAIN_DEPENDENCY "${SOURCE_FILE_PATH}"
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different "${SOURCE_FILE_PATH}" "${ARG_DESTINATION}/${DEST_FILE_NAME}"
-            COMMENT "Copying ${CMAKE_CURRENT_BINARY_DIR}/${ARG_DESTINATION}/${DEST_FILE_NAME}"
+    if(NOT ARG_DESTINATION)
+        set(ARG_DESTINATION "${CMAKE_CURRENT_BINARY_DIR}")
+    endif()
+
+    if(MSVC OR CMAKE_GENERATOR STREQUAL "Ninja Multi-Config")
+        string(REPLACE "src" "src/$<CONFIG>" output_dir "${ARG_DESTINATION}")
+    else()
+        set(output_dir "${ARG_DESTINATION}")
+    endif()
+
+    foreach(source_file ${ARG_SOURCE})
+        add_custom_command(OUTPUT "${output_dir}/${source_file}"
+            MAIN_DEPENDENCY "${CMAKE_CURRENT_SOURCE_DIR}/${source_file}"
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            "${CMAKE_CURRENT_SOURCE_DIR}/${source_file}"
+            "${output_dir}/${source_file}"
+            COMMENT "Copying ${output_dir}/${source_file}"
         )
-        list(APPEND DESTINATION_FILES "${CMAKE_CURRENT_BINARY_DIR}/${ARG_DESTINATION}/${DEST_FILE_NAME}")
+        list(APPEND destination_files "${output_dir}/${source_file}")
     endforeach()
 
-    add_custom_target(${ARG_NAME} DEPENDS ${DESTINATION_FILES})
-    set_target_properties(${ARG_NAME} PROPERTIES
-        OUT_FILES "${DESTINATION_FILES}"
+    add_custom_target(${target_name} DEPENDS ${destination_files})
+    set_target_properties(${target_name} PROPERTIES
+        OUT_FILES "${destination_files}"
     )
 endfunction()
 
 include(CheckCCompilerFlag)
 
+# add_compile_options_checked(<option> ...)
 # Don't support generator expressions
 function(add_compile_options_checked)
     foreach(flag ${ARGV})
@@ -40,6 +51,7 @@ function(add_compile_options_checked)
     add_compile_options(${flag_list})
 endfunction()
 
+# configure_empty_git_info(<Template> <Output>)
 function(configure_empty_git_info Template Output)
     set(Tag "<unknown>")
     set(Hash "<unknown>")
