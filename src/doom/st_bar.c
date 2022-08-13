991177bd1352abd6ea576b96fa72289fabb9e18c
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2022-08-13 10:30:01 +0300

    Doom: fix incorrect demo timer length for multiple players demo

--- src/doom/st_bar.c
+++ src/doom/st_bar.c
@@ -2045,7 +2045,17 @@ void ST_WidgetsDrawer (void)
     // [crispy] demo timer widget
     if (demoplayback && (demotimer == 1 || demotimer == 3))
     {
-        ST_DrawDemoTimer(demotimerdir ? (deftotaldemotics - defdemotics) : defdemotics);
+        int j = 0;
+
+        for (int i = 0 ; i < MAXPLAYERS ; i++)
+        {
+            if (playeringame[i])
+            {
+                j++;
+            }
+        }
+
+        ST_DrawDemoTimer(demotimerdir ? (deftotaldemotics - defdemotics) / j : defdemotics / j);
     }
     else if (demorecording && (demotimer == 2 || demotimer == 3))
     {
