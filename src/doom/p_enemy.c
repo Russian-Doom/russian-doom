a5142d9bf8428f356acc27989384aed6d5548f5f
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2022-08-16 22:54:55 +0300

    Doom: remove Icon of Sin explosion centering fix
    Probably it's not safe at all...

--- src/doom/p_enemy.c
+++ src/doom/p_enemy.c
@@ -2194,11 +2194,8 @@ void A_BrainPain (mobj_t *mo)
 void A_BrainScream (mobj_t *mo)
 {
     int x;
-    // [JN] Safe fix for centering Icon of Sin death explosions.
-    // https://doomwiki.org/wiki/Lopsided_final_boss_explosions
-    const int safe_x = canmodify && singleplayer && !strict_mode && !vanillaparm ? 320 : 196;
 
-    for (x=mo->x - safe_x*FRACUNIT ; x< mo->x + 320*FRACUNIT ; x+= FRACUNIT*8)
+    for (x=mo->x - 196*FRACUNIT ; x< mo->x + 320*FRACUNIT ; x+= FRACUNIT*8)
     {
         int y = mo->y - 320*FRACUNIT;
         int z = 128 + P_Random()*2*FRACUNIT;
