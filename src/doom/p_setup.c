278b8f4ec74784fcedcd91429325686eb78623f0
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2022-09-09 09:18:19 +0300

    Doom: simplify map name finding

--- src/doom/p_setup.c
+++ src/doom/p_setup.c
@@ -2188,24 +2188,13 @@ void P_SetupLevel (const int episode, const int map, const skill_t skill)
     W_Reload ();
 
     // find map name
-    if ( gamemode == commercial)
+    if (gamemode == commercial)
     {
-        if (map<10)
-        {
-            DEH_snprintf(lumpname, 9, "map0%i", map);
-        }
-        else
-        {
-            DEH_snprintf(lumpname, 9, "map%i", map);
-        }
+        DEH_snprintf(lumpname, 9, "MAP%02d", map);
     }
     else
     {
-        lumpname[0] = 'E';
-        lumpname[1] = '0' + episode;
-        lumpname[2] = 'M';
-        lumpname[3] = '0' + map;
-        lumpname[4] = 0;
+        DEH_snprintf(lumpname, 9, "E%dM%d", episode, map);
     }
 
     lumpnum = W_GetNumForName (lumpname);
