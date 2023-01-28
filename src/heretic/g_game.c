d825bb1d292db9ca7ebebcf6cdbefc88f4e7fa1f
Dasperal <dasperal1@gmail.com>
2023-01-28 18:59:00 +0300

    Heretic: Fix `bk_look_center` not working properly while recording a demo

--- src/heretic/g_game.c
+++ src/heretic/g_game.c
@@ -560,18 +560,22 @@ void G_BuildTiccmd(ticcmd_t *cmd, int maketic)
     {
         if (demorecording || lowres_turn)
         {
-            // [crispy] Map mouse movement to look variable when recording
-            look += mouse_y_invert ? -mousey / MLOOKUNITLOWRES
-                                   :  mousey / MLOOKUNITLOWRES;
+            // [Dasperal] Skip mouse look if it is TOCENTER cmd
+            if(look != TOCENTER)
+            {
+                // [crispy] Map mouse movement to look variable when recording
+                look += mouse_y_invert ? -mousey / MLOOKUNITLOWRES
+                                       :  mousey / MLOOKUNITLOWRES;
 
-            // [crispy] Limit to max speed of keyboard look up/down
-            if (look > 2)
-            {
-                look = 2;
-            }
-            else if (look < -2)
-            {
-                look = -2;
+                // [crispy] Limit to max speed of keyboard look up/down
+                if(look > 2)
+                {
+                    look = 2;
+                }
+                else if(look < -2)
+                {
+                    look = -2;
+                }
             }
         }
         else
