e3fa7243be2e846329e908fbb81f2a333a42a449
Dasperal <dasperal1@gmail.com>
2023-05-06 00:18:44 +0300

    Cmake: Add overlay triplets for VCPKG to fix broken defaults of `sdl2-mixer` and performance problems of `libsamplerate`
    CI: Remove git pull in VCPKG setup. Use more stable package versions

---
+++ cmake/triplets/x86-windows.cmake
@@ -0,0 +1,11 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE release)
+
+if(PORT STREQUAL "libsamplerate")
+    list(APPEND VCPKG_CXX_FLAGS "/fp:fast")
+    list(APPEND VCPKG_C_FLAGS "/fp:fast")
+elseif(PORT STREQUAL "sdl2-mixer")
+    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DSDL2MIXER_MIDI=ON" "-DSDL2MIXER_MIDI_TIMIDITY=ON")
+endif()
