b832a381b1711c29a53d26762ec347eed9156c2f
Dasperal <dasperal1@gmail.com>
2023-05-10 19:41:00 +0300

    Cmake: Update Find module
    Find DLLs. Find debug and static versions of libraries.
    
    For SDL2_mixer find optional DLLs and store their paths in the `OPTIONAL_DLLS` property of `SDL2_mixer::SDL2_mixer` target

--- cmake/FindSampleRate.cmake
+++ cmake/FindSampleRate.cmake
@@ -36,13 +36,14 @@ set(SAMPLERATE_DIR "${SAMPLERATE_DIR}" CACHE PATH "Location of Samplerate librar
 
 # Use pkg-config to find library locations in *NIX environments.
 find_package(PkgConfig QUIET)
-if(PKG_CONFIG_FOUND)
+if(PkgConfig_FOUND)
     pkg_search_module(PC_SAMPLERATE QUIET samplerate)
 endif()
 
 # Find the include directory.
 find_path(SAMPLERATE_INCLUDE_DIR "samplerate.h"
-    HINTS "${SAMPLERATE_DIR}/include" ${PC_SAMPLERATE_INCLUDE_DIRS})
+    PATH_SUFFIXES include
+    HINTS "${SAMPLERATE_DIR}" ${PC_SAMPLERATE_INCLUDE_DIRS})
 
 # Find the version.  I don't know if there is a correct way to find this on
 # Windows - the config.h in the tarball is wrong for 0.1.19.
@@ -50,22 +51,221 @@ if(PC_SAMPLERATE_VERSION)
     set(SAMPLERATE_VERSION "${PC_SAMPLERATE_VERSION}")
 endif()
 
-# Find the library.
-find_library(SAMPLERATE_LIBRARY "samplerate"
-    HINTS "${SAMPLERATE_DIR}/lib" ${PC_SAMPLERATE_LIBRARY_DIRS})
+# Find DLLs
+find_file(SAMPLERATE_DLL_RELEASE
+    NAMES samplerate.dll samplerate-0.dll libsamplerate.dll libsamplerate-0.dll
+    PATH_SUFFIXES bin
+    HINTS ${SAMPLERATE_DIR} "${PC_SAMPLERATE_PREFIX}"
+)
+find_file(SAMPLERATE_DLL_DEBUG
+    NAMES samplerate.dll samplerate-0.dll libsamplerate.dll libsamplerate-0.dll
+    PATH_SUFFIXES "debug/bin" bin
+    HINTS ${SAMPLERATE_DIR} "${PC_SAMPLERATE_PREFIX}"
+)
+
+include(SelectDllConfigurations)
+select_dll_configurations(SAMPLERATE)
+
+if(SAMPLERATE_DLL)
+    set(_samplerate_shared_release_names "samplerate.lib" "libsamplerate.dll.a")
+    set(_samplerate_static_release_names "samplerate-static.lib")
+    set(_samplerate_shared_debug_names "samplerate.lib")
+    set(_samplerate_static_debug_names "samplerate-staticd.lib")
+else()
+    set(_samplerate_shared_release_names "")
+    set(_samplerate_static_release_names "samplerate.lib" "samplerate-static.lib")
+    set(_samplerate_shared_debug_names "")
+    set(_samplerate_static_debug_names "samplerate.lib" "samplerate-staticd.lib")
+endif()
+
+set(_saved_suffixes ${CMAKE_FIND_LIBRARY_SUFFIXES})
+# Find the samplerate dynamic libraries
+set(CMAKE_FIND_LIBRARY_SUFFIXES "" ".so" ".dylib" ".dll.a")
+find_library(SAMPLERATE_LIBRARY_RELEASE
+    NAMES ${_samplerate_shared_release_names} samplerate
+    PATH_SUFFIXES lib
+    HINTS ${SAMPLERATE_DIR} "${PC_SAMPLERATE_LIBRARY_DIRS}"
+)
+find_library(SAMPLERATE_LIBRARY_DEBUG
+    NAMES ${_samplerate_shared_debug_names} samplerate
+    PATH_SUFFIXES "debug/lib" lib
+    HINTS ${SAMPLERATE_DIR} "${PC_SAMPLERATE_LIBRARY_DIRS}"
+)
+
+# Find the samplerate static libraries
+set(CMAKE_FIND_LIBRARY_SUFFIXES "" ".a")
+find_library(SAMPLERATE_STATIC_LIBRARY_RELEASE
+    NAMES ${_samplerate_static_release_names} samplerate
+    PATH_SUFFIXES lib
+    HINTS ${SAMPLERATE_DIR} "${PC_SAMPLERATE_LIBRARY_DIRS}"
+)
+find_library(SAMPLERATE_STATIC_LIBRARY_DEBUG
+    NAMES ${_samplerate_static_debug_names} samplerate
+    PATH_SUFFIXES "debug/lib" lib
+    HINTS ${SAMPLERATE_DIR} "${PC_SAMPLERATE_LIBRARY_DIRS}"
+)
+set(CMAKE_FIND_LIBRARY_SUFFIXES ${_saved_suffixes})
+
+unset(_saved_suffixes)
+
+# Select libraries
+include(SelectLibraryConfigurations)
+select_library_configurations(SAMPLERATE)
+select_library_configurations(SAMPLERATE_STATIC)
+
+get_flags_from_pkg_config("SHARED" "PC_SAMPLERATE" "_samplerate")
+get_flags_from_pkg_config("STATIC" "PC_SAMPLERATE" "_samplerate_static")
+
+# Link flags for samplerate static library if PkgConfig not found
+if(SAMPLERATE_STATIC_LIBRARY AND NOT PC_SAMPLERATE_FOUND)
+    if(NOT SAMPLERATE_LIBRARY)
+        set(SAMPLERATE_STATIC_LINK_LIBRARIES "" CACHE STRING "Additional libraries to link to samplerate-static.")
+        set(SAMPLERATE_STATIC_LINK_DIRECTORIES "" CACHE PATH "Additional directories to search libraries in for samplerate-static.")
+        set(_samplerate_static_link_libraries ${SAMPLERATE_STATIC_LINK_LIBRARIES})
+        set(_samplerate_static_link_directories ${SAMPLERATE_STATIC_LINK_DIRECTORIES})
+        if(NOT _samplerate_static_link_libraries)
+            message(WARNING
+                "pkg-config is unavailable and only a static version of samplerate was found.\n"
+                "Link failures are to be expected.\n"
+                "Set `SAMPLERATE_STATIC_LINK_LIBRARIES` to a list of libraries samplerate depends on.\n"
+                "Set `SAMPLERATE_STATIC_LINK_DIRECTORIES` to a list of directories to search for libraries in."
+            )
+        endif()
+    endif()
+endif()
+
+if(SAMPLERATE_LIBRARY)
+    set(_SAMPLERATE_LIBRARY "${SAMPLERATE_LIBRARY}")
+else()
+    set(_SAMPLERATE_LIBRARY "${SAMPLERATE_STATIC_LIBRARY}")
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SampleRate
     FOUND_VAR SAMPLERATE_FOUND
-    REQUIRED_VARS SAMPLERATE_INCLUDE_DIR SAMPLERATE_LIBRARY
+    REQUIRED_VARS SAMPLERATE_INCLUDE_DIR _SAMPLERATE_LIBRARY
     VERSION_VAR SAMPLERATE_VERSION
 )
+unset(_SDL2_LIBRARY)
 
-if(SAMPLERATE_FOUND)
-    # Imported target.
-    add_library(SampleRate::samplerate UNKNOWN IMPORTED)
-    set_target_properties(SampleRate::samplerate PROPERTIES
-        INTERFACE_COMPILE_OPTIONS "${PC_SAMPLERATE_CFLAGS_OTHER}"
-        INTERFACE_INCLUDE_DIRECTORIES "${SAMPLERATE_INCLUDE_DIR}"
-        IMPORTED_LOCATION "${SAMPLERATE_LIBRARY}")
+# Cleanup macro
+macro(_cleanup)
+    unset(_samplerate_shared_release_names)
+    unset(_samplerate_static_release_names)
+    unset(_samplerate_shared_debug_names)
+    unset(_samplerate_static_debug_names)
+    unset(_samplerate_compile_options)
+    unset(_samplerate_link_libraries)
+    unset(_samplerate_link_directories)
+    unset(_samplerate_link_options)
+    unset(_samplerate_static_compile_options)
+    unset(_samplerate_static_link_libraries)
+    unset(_samplerate_static_link_directories)
+    unset(_samplerate_static_link_options)
+    mark_as_advanced(FORCE
+        SAMPLERATE_INCLUDE_DIR
+        SAMPLERATE_DLL_RELEASE
+        SAMPLERATE_DLL_DEBUG
+        SAMPLERATE_LIBRARY_RELEASE
+        SAMPLERATE_LIBRARY_DEBUG
+        SAMPLERATE_STATIC_LIBRARY_RELEASE
+        SAMPLERATE_STATIC_LIBRARY_DEBUG
+        )
+endmacro()
+
+if(NOT SAMPLERATE_FOUND)
+    _cleanup()
+    return()
 endif()
+
+# samplerate imported target
+if(SAMPLERATE_LIBRARY)
+    add_library(SampleRate::samplerate SHARED IMPORTED)
+    set_target_properties(SampleRate::samplerate PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${SAMPLERATE_INCLUDE_DIR}"
+        INTERFACE_COMPILE_OPTIONS "${_samplerate_compile_options}"
+        INTERFACE_LINK_LIBRARIES "${_samplerate_link_libraries}"
+        INTERFACE_LINK_DIRECTORIES "${_samplerate_link_directories}"
+        INTERFACE_LINK_OPTIONS "${_samplerate_link_options}"
+    )
+    if(SAMPLERATE_DLL)
+        set_target_properties(SampleRate::samplerate PROPERTIES
+            IMPORTED_LOCATION "${SAMPLERATE_DLL}"
+            IMPORTED_IMPLIB "${SAMPLERATE_LIBRARY}"
+        )
+    else()
+        set_target_properties(SampleRate::samplerate PROPERTIES
+            IMPORTED_LOCATION "${SAMPLERATE_LIBRARY}"
+        )
+    endif()
+    if(SAMPLERATE_LIBRARY_RELEASE)
+        set_property(TARGET SampleRate::samplerate APPEND PROPERTY
+            IMPORTED_CONFIGURATIONS RELEASE
+        )
+        set_target_properties(SampleRate::samplerate PROPERTIES
+            MAP_IMPORTED_CONFIG_MINSIZEREL RELEASE
+            MAP_IMPORTED_CONFIG_RELWITHDEBINFO RELEASE
+        )
+        if(SAMPLERATE_DLL_RELEASE)
+            set_target_properties(SampleRate::samplerate PROPERTIES
+                IMPORTED_LOCATION_RELEASE "${SAMPLERATE_DLL_RELEASE}"
+                IMPORTED_IMPLIB_RELEASE "${SAMPLERATE_LIBRARY_RELEASE}"
+            )
+        else()
+            set_target_properties(SampleRate::samplerate PROPERTIES
+                IMPORTED_LOCATION_RELEASE "${SAMPLERATE_LIBRARY_RELEASE}"
+            )
+        endif()
+    endif()
+    if(SAMPLERATE_LIBRARY_DEBUG)
+        set_property(TARGET SampleRate::samplerate APPEND PROPERTY
+            IMPORTED_CONFIGURATIONS DEBUG
+        )
+        if(SAMPLERATE_DLL_DEBUG)
+            set_target_properties(SampleRate::samplerate PROPERTIES
+                IMPORTED_LOCATION_DEBUG "${SAMPLERATE_DLL_DEBUG}"
+                IMPORTED_IMPLIB_DEBUG "${SAMPLERATE_LIBRARY_DEBUG}"
+            )
+        else()
+            set_target_properties(SampleRate::samplerate PROPERTIES
+                IMPORTED_LOCATION_DEBUG "${SAMPLERATE_LIBRARY_DEBUG}"
+            )
+        endif()
+    endif()
+endif()
+
+# samplerate-static imported target
+if(SAMPLERATE_STATIC_LIBRARY)
+    add_library(SampleRate::samplerate-static STATIC IMPORTED)
+    set_target_properties(SampleRate::samplerate-static PROPERTIES
+        IMPORTED_LOCATION "${SAMPLERATE_STATIC_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${SAMPLERATE_INCLUDE_DIR}"
+        INTERFACE_COMPILE_OPTIONS "${_samplerate_static_compile_options}"
+        INTERFACE_LINK_LIBRARIES "${_samplerate_static_link_libraries}"
+        INTERFACE_LINK_DIRECTORIES "${_samplerate_static_link_directories}"
+        INTERFACE_LINK_OPTIONS "${_samplerate_static_link_options}"
+    )
+    if(SAMPLERATE_STATIC_LIBRARY_RELEASE)
+        set_property(TARGET SampleRate::samplerate-static APPEND PROPERTY
+            IMPORTED_CONFIGURATIONS RELEASE
+        )
+        set_target_properties(SampleRate::samplerate-static PROPERTIES
+            IMPORTED_LOCATION_RELEASE "${SAMPLERATE_STATIC_LIBRARY_RELEASE}"
+            MAP_IMPORTED_CONFIG_MINSIZEREL RELEASE
+            MAP_IMPORTED_CONFIG_RELWITHDEBINFO RELEASE
+        )
+    endif()
+    if(SAMPLERATE_STATIC_LIBRARY_DEBUG)
+        set_property(TARGET SampleRate::samplerate-static APPEND PROPERTY
+            IMPORTED_CONFIGURATIONS DEBUG
+        )
+        set_target_properties(SampleRate::samplerate-static PROPERTIES
+            IMPORTED_LOCATION_DEBUG "${SAMPLERATE_STATIC_LIBRARY_DEBUG}"
+        )
+    endif()
+    if(NOT TARGET SampleRate::samplerate)
+        add_library(SampleRate::samplerate ALIAS SampleRate::samplerate-static)
+    endif()
+endif()
+
+_cleanup()
--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,4 +1,5 @@
-# add_copy_target(<target_name> SOURCE <file1> <...> [DESTINATION <dir>])
+# add_copy_target(<target_name> SOURCE <file> ... [DESTINATION <dir>])
+# Copy given files to given dir at build time
 function(add_copy_target target_name)
     cmake_parse_arguments(PARSE_ARGV 1 "ARG"
         ""            # List of Options
@@ -31,6 +32,35 @@ function(add_copy_target target_name)
     set_target_properties(${target_name} PROPERTIES
         OUT_FILES "${destination_files}"
     )
+endfunction()
+
+# copy_files(SOURCE <file> ... [DESTINATION <dir>])
+# Copy given files to given dir at config time
+function(copy_files)
+    cmake_parse_arguments(PARSE_ARGV 0 "ARG"
+        ""            # List of Options
+        "DESTINATION" # List of Values
+        "SOURCE"      # List of Lists
+    )
+    if(NOT ARG_SOURCE)
+        return()
+    endif()
+    if(NOT ARG_DESTINATION)
+        set(ARG_DESTINATION "${CMAKE_CURRENT_BINARY_DIR}")
+    endif()
+
+    if(MSVC OR CMAKE_GENERATOR STREQUAL "Ninja Multi-Config")
+        foreach(_config ${CMAKE_CONFIGURATION_TYPES})
+            string(REPLACE "src" "src/${_config}" output_dir_config "${ARG_DESTINATION}")
+            list(APPEND output_dirs "${output_dir_config}")
+        endforeach()
+    else()
+        set(output_dirs "${ARG_DESTINATION}")
+    endif()
+
+    foreach(output_dir ${output_dirs})
+        file(COPY ${ARG_SOURCE} DESTINATION "${output_dir}")
+    endforeach()
 endfunction()
 
 include(CheckCCompilerFlag)
@@ -74,4 +104,55 @@ function(target_common_settings Target)
         INTERPROCEDURAL_OPTIMIZATION ${ENABLE_LTO}
         INTERPROCEDURAL_OPTIMIZATION_DEBUG OFF
     )
+endfunction()
+
+# Helper for Find modules.
+# get_flags_from_pkg_config(<STATIC|SHARED> <pc_prefix> <out_prefix>)
+function(get_flags_from_pkg_config _library_type _pc_prefix _out_prefix)
+    if(NOT ${_pc_prefix}_FOUND)
+        set(${_out_prefix}_compile_options
+            ""
+            PARENT_SCOPE)
+        set(${_out_prefix}_link_libraries
+            ""
+            PARENT_SCOPE)
+        set(${_out_prefix}_link_options
+            ""
+            PARENT_SCOPE)
+        set(${_out_prefix}_link_directories
+            ""
+            PARENT_SCOPE)
+        return()
+    endif()
+
+    if("${_library_type}" STREQUAL "STATIC")
+        set(_cflags ${_pc_prefix}_STATIC_CFLAGS_OTHER)
+        set(_link_libraries ${_pc_prefix}_STATIC_LIBRARIES)
+        set(_link_options ${_pc_prefix}_STATIC_LDFLAGS_OTHER)
+        set(_library_dirs ${_pc_prefix}_STATIC_LIBRARY_DIRS)
+    else()
+        set(_cflags ${_pc_prefix}_CFLAGS_OTHER)
+        set(_link_libraries ${_pc_prefix}_LIBRARIES)
+        set(_link_options ${_pc_prefix}_LDFLAGS_OTHER)
+        set(_library_dirs ${_pc_prefix}_LIBRARY_DIRS)
+    endif()
+
+    # The *_LIBRARIES lists always start with the library itself
+    list(REMOVE_AT "${_link_libraries}" 0)
+
+    # Work around CMake's flag deduplication when pc files use `-framework A` instead of `-Wl,-framework,A`
+    string(REPLACE "-framework;" "-Wl,-framework," "_filtered_link_options" "${${_link_options}}")
+
+    set(${_out_prefix}_compile_options
+        "${${_cflags}}"
+        PARENT_SCOPE)
+    set(${_out_prefix}_link_libraries
+        "${${_link_libraries}}"
+        PARENT_SCOPE)
+    set(${_out_prefix}_link_options
+        "${_filtered_link_options}"
+        PARENT_SCOPE)
+    set(${_out_prefix}_link_directories
+        "${${_library_dirs}}"
+        PARENT_SCOPE)
 endfunction()
