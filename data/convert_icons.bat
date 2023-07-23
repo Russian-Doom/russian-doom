62f1171ca32a28d5a0cde96e055626b2ffc7cd62
Dasperal <dasperal1@gmail.com>
2023-07-23 18:19:00 +0300

    Remove the rest of Strife files

--- data/convert_icons.bat
+++ data/convert_icons.bat
@@ -1,5 +1,3 @@
 python convert-icon.py doom.png doom_icon.c
 python convert-icon.py heretic.png heretic_icon.c
 python convert-icon.py hexen.png hexen_icon.c
-python convert-icon.py strife.png strife_icon.c
-python convert-icon.py setup.png setup_icon.c
