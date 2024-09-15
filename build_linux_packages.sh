e13ad095235dbd82ac6d00a0dfabbad17e043dd6
Dasperal <dasperal1@gmail.com>
2024-09-15 19:02:49 +0300

    Cmake: Add Cpack configuration and presets for building Fedora RPM packages

--- build_linux_packages.sh
+++ build_linux_packages.sh
@@ -6,3 +6,5 @@ bash ./build_toolchain_images.sh
 # Execute cmake in workflow mode
 docker run --rm -v ".:/tmp/russian-doom" toolchain-russian-doom-debian \
        /bin/bash -c "cd /tmp/russian-doom && cmake --workflow --preset 'debian-linux-release'"
+docker run --rm -v ".:/tmp/russian-doom" toolchain-russian-doom-fedora \
+       /bin/bash -c "cd /tmp/russian-doom && cmake --workflow --preset 'fedora-linux-release'"
